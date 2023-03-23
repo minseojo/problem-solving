baaf2ae3fcdb4122d1acb4a40f6b16e6038b0900
Minseo  Jo <liging12@naver.com>
2023-03-23 22:48:07 +0900

    [Silver III] Title: 두 수의 합, Time: 760 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[100000];
+
+int main() {
+	int n, x;
+	cin >> n;
+	for(int i=0; i<n; i++) {
+		cin >> a[i];
+	}
+	cin >> x;
+	sort(a, a+n);
+	
+	int l=0, r=n-1;
+	int result = 0;
+	while(r>0 && l<r) {
+		int sum = a[l]+a[r];
+		if(sum <= x) {
+			if(sum == x) result++;
+			l++;
+		}
+		else if(sum > x) {
+			r--;
+			l=0;
+		}
+	}
+	cout << result;        
+}
