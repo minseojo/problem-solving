8e9704d611c1559a1e299d262857ade4131b6c17
Minseo  Jo <liging12@naver.com>
2023-03-30 20:25:56 +0900

    [Silver III] Title: 비슷한 단어, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int alpha_copy[26];
+string first;
+
+bool compareAlpha(string second) {
+	int fs = first.length();
+	int ss = second.length();
+	if(abs(fs-ss) > 1) return false;
+	
+	int alpha[26];
+	int dupl_and_dif = 0;
+	int size = 0;
+	for(int i=0; i<26; i++) {
+		alpha[i] = alpha_copy[i];
+	}
+	for(int i=0; i<second.length(); i++) {
+		int idx = second[i]-'A';
+		
+		if(alpha[idx] > 0) {
+			alpha[idx]--;
+			size++;
+		} else if(alpha[idx] == 0) {
+			dupl_and_dif++;
+		}
+		if(dupl_and_dif > 1) return false;
+	}
+	if(fs-ss == 1 && dupl_and_dif == 1) return false;
+	return true;
+}
+int main() {
+	int n;
+	cin >> n;
+	if(n > 0) cin >> first;
+	for(int i=0; i<first.length(); i++) {
+		alpha_copy[first[i]-'A']++;
+	}
+
+	int answer=0;
+	for(int i=0; i<n-1; i++) {
+		string second;
+		cin >> second;
+
+		if(compareAlpha(second)) {
+			answer++;
+		}
+	}
+	cout << answer;
+}
