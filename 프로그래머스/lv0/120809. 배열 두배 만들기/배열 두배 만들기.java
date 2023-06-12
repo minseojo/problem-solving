364be3fc0edaceac5d9ccbf81d0fd9c9adf2fd7f
Minseo  Jo <liging12@naver.com>
2023-06-13 02:31:33 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.87 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
-        int i = 0;
-        for(int number : numbers) {
-            answer[i++] = number*2;
-        }
-        return answer;
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
 }
