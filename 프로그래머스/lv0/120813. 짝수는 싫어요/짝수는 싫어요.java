df053bc73b8b8b7bf967d4769eab5f58d852c16a
Minseo  Jo <liging12@naver.com>
2023-06-14 02:02:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.71 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,13 +1,14 @@
+import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        int[] answer;
-        if (n%2 == 0) answer = new int[n/2];
-        else answer = new int[n/2 + 1];
-
-        int pos = 0;
+        List<Integer> result = new ArrayList<>();
         for(int i=1; i<=n; i+=2) {
-            answer[pos++] = i;
+            result.add(i);
         }
-        return answer;
+        
+        return result
+            .stream()
+            .mapToInt(i->i)
+            .toArray();
     }
 }
