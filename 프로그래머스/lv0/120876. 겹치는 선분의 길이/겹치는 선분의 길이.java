82cb0c18c0dd13ceba5c8fb7ea21b196187a9af5
Minseo  Jo <liging12@naver.com>
2023-06-13 23:22:37 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -13,28 +13,3 @@ class Solution {
         return answer;
     }
 }
-
-/*
-class Solution {
-    public int solution(int[][] lines) {
-        int answer = 0;
-        int[] line = new int[202];
-        for (int i=0; i<lines.length; i++) {
-            for (int j=lines[i][0]; j<=lines[i][1]; j++) {
-                line[j+100]++;
-            }
-        }
-        
-        int sequence = 0;
-        for(int i=0; i<202; i++) {
-            System.out.print(line[i]);
-            if(line[i] > 1) sequence++;
-            else if(sequence > 0 && line[i] < 2) {
-                answer += (sequence-1);
-                sequence = 0;
-            }
-        }
-        return answer;
-    }
-}
-*/
