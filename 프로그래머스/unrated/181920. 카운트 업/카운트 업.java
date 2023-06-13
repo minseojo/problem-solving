0f4e47069c747b97665f55332183fd4d19ac478d
Minseo  Jo <liging12@naver.com>
2023-06-13 19:35:40 +0900

    [unrated] Title: 카운트 업, Time: 1.73 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,10 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int start, int end) {
-        int[] answer = new int[end-start+1];
-        int pos=0;
-        for(int i=start; i<=end; i++) {
-            answer[pos++] = i;
-        }
-        return answer;
+        return IntStream.rangeClosed(start, end).toArray();
     }
 }
