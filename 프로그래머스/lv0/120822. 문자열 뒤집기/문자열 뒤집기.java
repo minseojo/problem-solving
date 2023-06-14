7ef8428369cba14e8f868d4b4dcf25556ab56841
Minseo  Jo <liging12@naver.com>
2023-06-15 01:04:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.02 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i = my_string.length()-1; i >= 0; i--) {
-            answer += my_string.charAt(i);
-        }
-        return answer;
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
+
+        return sb.toString();
     }
 }
