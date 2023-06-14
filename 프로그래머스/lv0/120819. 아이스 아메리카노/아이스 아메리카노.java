eb192259a7ee5222c80fb385f4a04aa716a6af6d
Minseo  Jo <liging12@naver.com>
2023-06-15 01:01:47 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,5 @@
 class Solution {
-    private final static int iced_americano = 5500;
     public int[] solution(int money) {
-        return new int[]{money / iced_americano, money - iced_americano *(money / iced_americano)};
+        return new int[]{money / 5500, money % 5500};
     }
 }
