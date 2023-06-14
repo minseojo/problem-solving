5ae040182dcf0f506b8dcfae8993fdbf8f0e02db
Minseo  Jo <liging12@naver.com>
2023-06-15 00:34:02 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 9.56 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -0,0 +1,36 @@
+class Solution {
+    int[] dy = {1, 0, -1, 0};
+    int[] dx = {0, -1, 0, 1};
+    public int[][] solution(int n) {
+        int[][] map = new int[n][n];
+        int y = 0, x = 0, cnt = 1;
+        int dir = 0;
+
+        for(int i=0; i<n; i++) {
+            map[0][i] = cnt++;
+            x++;
+        }
+        x--;
+        
+        while (n > 0) {
+            int size = n-1;
+            while(size > 0) {
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                    
+                
+      
+                map[ny][nx] = cnt++;
+                y += dy[dir];
+                x += dx[dir];
+                size--;
+            }
+            dir++;
+            dir %= 4;
+            if(dir == 0 || dir == 2) n--;
+            System.out.println(dir + " " + n);
+        }
+        
+        return map;
+    }
+}
