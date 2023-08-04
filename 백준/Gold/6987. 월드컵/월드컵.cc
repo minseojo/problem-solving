e7f8eb09fe57c218befda820eb230a466f321e79
Minseo  Jo <liging12@naver.com>
2023-08-04 13:24:24 +0900

    [Gold IV] Title: 월드컵, Time: 260 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/6987. 월드컵/월드컵.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int WIN = 0;
+const int DRAW = 1;
+const int LOSE = 2;
+
+struct Match {
+    int teamA;
+    int teamB;
+};
+
+
+const static int TOTAL_TEAM_COUNT = 6;
+const static int TOTAL_MATCH_COUNT = 15;
+vector<Match> matches = {
+    {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5},
+    {1, 2}, {1, 3}, {1, 4}, {1, 5},
+    {2, 3}, {2, 4}, {2, 5},
+    {3, 4}, {3, 5},
+    {4, 5}
+};
+
+int record[TOTAL_MATCH_COUNT][3];
+
+bool dfs(int matchCount, int cache[TOTAL_TEAM_COUNT][3]) {
+    if (matchCount == TOTAL_MATCH_COUNT) {
+        for (int i = 0; i < TOTAL_TEAM_COUNT; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (cache[i][j] != record[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    int teamA = matches[matchCount].teamA;
+    int teamB = matches[matchCount].teamB;
+    // teamA, teamB
+    // win, lose
+    cache[teamA][WIN]++;
+    cache[teamB][LOSE]++;
+    if (dfs(matchCount+1, cache)) return true;
+    cache[teamA][WIN]--;
+    cache[teamB][LOSE]--;
+
+    // draw, draw
+    cache[teamA][DRAW]++;
+    cache[teamB][DRAW]++;
+    if (dfs(matchCount+1, cache)) return true;
+    cache[teamA][DRAW]--;
+    cache[teamB][DRAW]--;
+
+    // lose, win
+    cache[teamA][LOSE]++;
+    cache[teamB][WIN]++;
+    if (dfs(matchCount+1, cache)) return true;
+    cache[teamA][LOSE]--;
+    cache[teamB][WIN]--;
+
+    return false;
+}
+
+vector<int> ans;
+int main() {
+    int tc = 4;
+    while(tc--) {
+        int cache[TOTAL_TEAM_COUNT][3] = {0}; 
+        for (int i = 0; i < TOTAL_TEAM_COUNT; i++) {
+            for (int j = 0; j < 3; j++) {
+                cin >> record[i][j];
+            }
+        }
+        ans.push_back(dfs(0, cache));
+    }
+
+    for (int answer : ans) {
+        cout << answer << " ";
+    }
+}
