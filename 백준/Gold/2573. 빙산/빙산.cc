f34c8a0b1f06ce8f6833ef1293f43d07c4774273
Minseo  Jo <liging12@naver.com>
2023-08-05 15:10:55 +0900

    [Gold IV] Title: 빙산, Time: 84 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,140 +1,107 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct st {
-	int y;
-	int x;
-	int d;
-};
-queue<st> q;
-
+int a[300][300];
+int copy_a[300][300]; 
 int n, m;
-int board[303][303];
-int copy_board[303][303];
-int dy[4] = {1,-1,0,0};
-int dx[4] = {0,0,1,-1};
-int ice_bergs = 0;
-bool visited[303][303];
 
-void copyBoard() { // 배열 복사 
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<m; j++) {
-			board[i][j] = copy_board[i][j];
-		}
-	}
-}
-
-bool isValidRange(int y, int x) { // 유효범위 체크 
-	if(y<0 || x<0 || y>n-1 || x>m-1) return false;
-	
-	return true;
-}
-
-void countIceBerg(int y, int x) { // 빙하 덩어리 개수 세기 
-	queue<pair<int, int>> check_q;
-	check_q.push({y, x});
-	while(!check_q.empty()) {
-		int y = check_q.front().first;
-		int x = check_q.front().second;
-
-		check_q.pop();
-		
-		for(int i=0; i<4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			
-			if(!isValidRange(ny, nx)) continue;
-			
-			if(!visited[ny][nx] && board[ny][nx] > 0) {
-				visited[ny][nx] = true;	
-				check_q.push({ny, nx});			
-			}
-		}
-	}
-}
-
-void melt() { // 빙하 녹이기 
-	int size = q.size();		
-
-	while(size--) {
-		int y = q.front().y;
-		int x = q.front().x;
-		int d = q.front().d;
-		q.pop();
-		
-		for(int i=0; i<4; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			
-			if(!isValidRange(ny, nx)) continue;
-				
-			if(d>0 && board[ny][nx] == 0) {
-				d--;
-			}
-		}
-
-		copy_board[y][x] = d;
-		if(d==0) ice_bergs -= 1;
-		else if(d>0) {
-			q.push({y, x, d});	
-		}
-	}
-	
-	copyBoard();
-} 
-
-void solve() {
-	int answer = 0;	
-	
-	while(1) {
-		memset(visited, 0, sizeof(visited));
-		int count = 0;
-		
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				if(board[i][j] > 0 && !visited[i][j]) {
-					visited[i][j] = true;
-					countIceBerg(i, j);
-					count += 1;
+void melt() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (a[i][j] > 0) {
+				// up
+				if ((i-1) >= 0) {
+					if (copy_a[i-1][j] <= 0) a[i][j]--;
+				}
+				// down
+				if ((i+1) < n) {
+					if (copy_a[i+1][j] <= 0) a[i][j]--;	
+				}
+				// left
+				if ((j-1) >= 0) {
+					if (copy_a[i][j-1] <= 0) a[i][j]--;
+				}
+				//right
+				if ((j+1) < m) {
+					if (copy_a[i][j+1] <= 0) a[i][j]--;
 				}
 			}
 		}
-		
-		if(count == 0) { // 다 녹았을 경우 
-			answer = 0;
-			break;
-		}
-		else if(count == 1) { // 한 덩어리 
-			melt();
-			answer += 1;
-		}
-		else if(count > 1) { // 두 덩어리 이상 
-			break;
-		}
 	}
-	
-	cout << answer;
+	for (int i = 0 ; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+		//	cout << a[i][j];
+			copy_a[i][j] = a[i][j];
+		}
+	//	cout << endl;
+	}
+
 }
 
-void init() {
-	cin >> n >> m;
-	for(int i=0; i<n; i++) {
-		for(int j=0; j<m; j++) {
-			cin >> copy_board[i][j];
- 
-			if(copy_board[i][j] > 0) {
-				ice_bergs += 1;
-				q.push({i, j, copy_board[i][j]});
-			}
-		}
-	}
+bool visited[300][300];
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0, 0, 1, -1};
+void check(int y, int x) {
+	visited[y][x] = true;
 	
-	copyBoard();
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx]) continue;
+		if (a[ny][nx] <= 0) continue;
+		check(ny, nx);
+	}
 }
 
 int main() {
-	init();
-	solve();
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+			copy_a[i][j] = a[i][j];
+		}
+	}
 	
-	return 0;	
+	int cnt= 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (!visited[i][j] && a[i][j] > 0) {
+				check(i, j);
+				cnt++;
+			}
+		}
+	}
+
+	// 빙산이 없거나, 이미 두동강 난 경우 
+	if (cnt == 0 || cnt > 1) {
+		cout << 0;
+		return 0;
+	}
+	
+	int ans = 0;
+	bool okMelt = false;
+	while(1) {
+		melt();
+		ans++;
+		memset(visited, 0, sizeof(visited));
+		cnt = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (!visited[i][j] && a[i][j] > 0) {
+					check(i, j);
+					cnt++;
+				}
+			}
+		}
+		if (cnt == 0) {
+			okMelt = false;
+			break;
+		}
+		if (cnt > 1) {
+			okMelt = true;
+			break;
+		}
+	}
+	if (okMelt) cout << ans << endl;
+	else cout << 0 << endl;
 }
