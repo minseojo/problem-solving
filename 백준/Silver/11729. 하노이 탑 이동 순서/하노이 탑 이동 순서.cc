850199040b3895487d7388cacae3bf4a99e4b56c
Minseo  Jo <liging12@naver.com>
2023-08-08 13:05:02 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 172 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -1,26 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 #define endl '\n'
+
 using namespace std;
 
-void solution(int n, int start, int temp, int end) {
-    if(n == 1) cout << start << ' ' << end << endl;
-    else {
-        solution(n-1, start, end, temp);
-		cout << start << ' ' << end << endl;
-        solution(n-1, temp, start, end);
+void hanoi(int n, int from, int mid, int to) {
+    if (n == 1) {
+        cout << from << " " << to << endl;
+    } else {
+        hanoi(n-1, from, to, mid);
+        cout << from << " " << to << endl;
+        hanoi(n-1, mid, from, to);
     }
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	int N, sum;
-	cin >> N;
-	sum = pow(2, N) - 1;
-	
-	cout << sum << endl;
-	solution(N, 1, 2, 3);
-	
-	return 0;
+    int n;
+    cin >> n;
+    cout << (int) (pow(2, n)-1) << endl;
+    hanoi(n, 1, 2, 3);
+    return 0;
 }
