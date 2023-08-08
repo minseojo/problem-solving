28125c88d580c0f4e6d96b95e590943b989ec59e
Minseo  Jo <liging12@naver.com>
2023-08-08 12:14:35 +0900

    [Gold III] Title: 줄 세우기, Time: 80 ms, Memory: 4692 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int DAG[32001];
+vector<int> graph[32001];
+int n, m;
+vector<int> start;
+
+void solve(int here) {
+	cout << here << " ";
+	for(int i=0; i<graph[here].size(); i++) {
+		int next = graph[here][i];
+		
+		DAG[next]--;
+		if(DAG[next] == 0) solve(next);
+	}
+}
+
+int main() {
+	cin >> n >> m;
+	
+	int a, b;
+	for(int i=0; i<m; i++) {
+		cin >> a >> b;
+		graph[a].push_back(b);
+		DAG[b]++;
+	}
+	
+	for(int i=1; i<=n; i++) {
+		if(DAG[i] == 0) start.push_back(i);
+	}
+
+	for(int i=0; i<start.size(); i++) {
+		solve(start[i]);
+	}
+	
+	return 0;
+}
