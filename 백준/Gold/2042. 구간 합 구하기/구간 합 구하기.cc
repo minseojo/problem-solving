19cff6f1517aa4b911bc50e51d2277dfd7269977
Minseo  Jo <liging12@naver.com>
2023-08-09 13:26:42 +0900

    [Gold I] Title: 구간 합 구하기, Time: 624 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,53 +1,59 @@
 #include <bits/stdc++.h>
-#define ll long long
 using namespace std;
 
-ll arr[1000001];
-ll tree[1000001 * 4];
+int N, M, K;
+long long leaf[1000001];
+long long tree[1000001 * 4];
 
-ll init(int start, int end, int node) {
-	if (start == end) return tree[node] = arr[start];	
-	int mid = (start+end)/2;
-	return tree[node] = init(start, mid, node*2) + init(mid+1, end, node*2+1);
+long long init(int start, int end, int node) {
+	if(start == end) return tree[node] = leaf[start];
+	
+	int mid = (start + end) / 2;
+	return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
 }
 
-ll sum(int start, int end, int node, int left, int right) {
-	if (left > end || start > right) return 0;
-	if (left <= start && end <= right) return tree[node];
-	int mid = (start+end)/2;
-	return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
+//left ~ right 합 구하는 구간 
+long long prefixSum (int start, int end, int node, int left, int right) { 
+	if(left > end || right < start) return 0;
+	if(left <= start && end <= right) return tree[node];
+	
+	int mid = (start + end) / 2;
+	return prefixSum(start, mid, node * 2, left, right) + prefixSum(mid + 1, end, node * 2 + 1, left, right); 
 }
 
-void update(int start, int end, int node, int index, ll diff) {
-	if (start > index || index > end) return;
+void update(int start, int end, int node, int index, long long diff) {
+	if(index < start || index > end) return;
 	
 	tree[node] += diff;
 	if(start == end) return;
-
-	int mid = (start+end)/2;
-	update(start, mid, node*2, index, diff);
-	update(mid+1, end, node*2+1, index, diff);
-}
+	
+	int mid = (start + end) / 2;
+	update(start, mid, node * 2, index, diff);
+	update(mid + 1, end, node * 2 + 1, index, diff);
+} 
 
 int main() {
-	int N, M, K;
-	cin >> N >> M >> K;
-	for(int i=1; i<=N; i++) cin >> arr[i];
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
 	
+	cin >> N >> M >> K;
+	
+	for(int i=1; i<=N; i++) {
+		cin >> leaf[i];
+	}
+
 	init(1, N, 1);
 	
-	for(int i=0; i<M+K; i++) {
-		ll a, b, c;
+	long long a, b, c;
+	for(int i = 0; i < M + K; i++) {
 		cin >> a >> b >> c;
-		if (a==1) {
-			ll diff = c-arr[b];
-			arr[b] = c;
+		if(a == 1) {
+			long long diff = c - leaf[b];
+			leaf[b] = c;
 			update(1, N, 1, b, diff);
-		}
-		else if (a==2) {
-			cout << sum(1, N, 1, b, c) << endl;
+		} else {
+			cout << prefixSum(1, N, 1, b, c) << endl;
 		}
 	}
-	
-	return 0;
 }
