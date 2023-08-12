d9295d76cb0b9afdb652dc96e1905d5772bebd7b
Minseo  Jo <liging12@naver.com>
2023-08-12 22:16:59 +0900

    [Gold V] Title: 회장뽑기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.cc
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.cc
@@ -1,55 +1,69 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dist[51][51];
+struct St {
+	int score;
+	int person;
+	bool operator< (const St& st) {
+		if (score == st.score) return person < st.person;
+		return score < st.score;
+	}
+};
+
+vector<int> adj[51];
+bool visited[51];
+vector<St> ans;
+vector<St> ans2;
+void bfs(int start) {
+	queue<St> q;
+	q.push({0, start});
+	visited[start] = true;
+	int result = 0;
+	while(!q.empty()) {
+		int size = q.size();
+		while(size--) {
+			int node = q.front().person;
+			int score = q.front().score;
+			result = max(result, score);
+			q.pop();
+			for (int i = 0; i < adj[node].size(); i++) {
+				int next_node = adj[node][i];
+				if (visited[next_node]) continue;
+				visited[next_node] = true;
+				q.push({score+1, next_node});
+			}
+		}
+	}
+	ans.push_back({result, start});
+}
 
 int main() {
 	int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++){
-            if(i == j) continue;
-            else dist[i][j] = 1e9;
-        }
-    }
-    
-    while(1) {
-    	int u, v;
-        cin >> u >> v;
-        if (u == -1 && v == -1) break;
-        dist[u][v] = 1;
-        dist[v][u] = 1;
-    }
-    
-    for (int k = 1; k <= n; k++) {
-    	for (int i = 1; i <= n; i++) {
-        	for (int j = 1; j <= n; j++) {
-            	dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);	
-            }
-		}	
+	cin >> n;
+	while(1) {
+		int u, v;
+		cin >> u >> v;
+		if(u == -1 && v == -1) break;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
 	}
-        
-    int result = 1e9;
-    vector<int> v;
-    for(int i = 1; i <= n; i++) {
-        int temp = 0;
-        for (int j = 1; j <= n; j++) {
-            temp = max(temp, dist[i][j]);
-        }
-        if (temp < result) {
-            v.clear();
-            result = temp;
-            v.push_back(i);
-        }
-        else if (temp == result) {
-            v.push_back(i);
-        }
-    }
-    
-    sort(v.begin(),v.end());
-    cout << result << " " << v.size() << endl;
-    for(int i=0; i < v.size(); i++) {
-        cout << v[i]<<" ";
-    }
-    return 0;
+	
+	for (int i = 1; i <= n; i++) {
+		memset(visited, 0, sizeof(visited));
+		bfs(i);
+	}
+	sort(ans.begin(), ans.end());
+	int max_score = ans[0].score;
+	
+	for (int i = 0; i < ans.size(); i++) {
+		if (max_score == ans[i].score) {
+			ans2.push_back({max_score, ans[i].person});
+		}
+	}
+
+	cout << max_score << " " << ans2.size() << endl;
+	for (St i: ans2) {
+		cout << i.person <<" ";
+	}
+	return 0;
 }
