530641908921162ab7f394a09cfc5da02a7c6a36
Minseo  Jo <liging12@naver.com>
2023-08-16 16:05:31 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,41 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int N, M;
-vector<int> A;
+const long long int MAX = 21000000000;
 
-int main() {
-	cin >> N >> M;
-	
-	int num;
-	for(int i = 0; i < N; i++) {
-		cin >> num;
-		A.push_back(num);
-	}
-	sort(A.begin(), A.end());
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int l = 0, r = 1;
-	long long sum = 0, answer = 1e10, dif = 1e10;
-	while(l < r && r < N) {
-		sum = A[r] - A[l];
-	
-		if(sum == M) {
-			dif = A[r]-A[l];
-			answer = min(answer, dif);
-			r++;
-		} 
-		else if(sum > M) {
-			dif = A[r]-A[l];
-			answer = min(answer, dif);
-			l++;
-		}
-		else if(sum < M) {
-			r++;
-		}
-		
-		if(l == r) r++;
-	}
-	
-	cout << answer;
-	return 0;
+    long long int n, m, left=0, right=1, temp, ans = MAX;
+    vector<int> arr;
+
+    cin >> n >> m;
+
+    for (int i=0; i<n; i++) {
+        cin >> temp;
+        arr.push_back(temp);
+    }
+
+    sort (arr.begin(), arr.end());
+
+    while (1) {
+        if (left >= n || right >= n) break;
+        temp = arr[right] - arr[left];
+        if (temp < m) right++;
+        else {
+            ans = min(ans, temp);
+            left++;
+        }
+    }
+
+    cout << ans << "\n";
+    return 0;
 }
