26b8219f77b9dba61f36b14250a44617a81de6af
Minseo  Jo <liging12@naver.com>
2023-08-16 17:06:27 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2388 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -16,28 +16,14 @@ int main() {
 			if (j == 0) { //  처음에는 항상 나무 1에 있음 
 				dp[i][j][1] = dp[i-1][j][1] + (a[i] == 1);
 			} else {
+				// 현재위치 -> 현재위치 (1->2->1, 2->1->2)  or  다른위치 -> 현재위치 (1->2, 2->1)
 				dp[i][j][1] = max(dp[i-1][j][1] + (a[i] == 1), dp[i-1][j-1][2] + (a[i] == 1));
 				dp[i][j][2] = max(dp[i-1][j][2] + (a[i] == 2), dp[i-1][j-1][1] + (a[i] == 2));
 			}
 		}
 	}
-	
-	int ans = 0;
-	for (int i = 1; i <= t; i++) {
-		for (int j =0; j<=w; j++) {
-			ans = max(ans, dp[i][j][1]);
-			ans = max(ans, dp[i][j][2]);
-		//	cout << dp[i][j][1] << " ";
-		}//cout << endl;
-	}
-	
-//	cout << endl;
-	for (int i = 1; i <= t; i++) {
-		for (int j =0; j<=w; j++) {
-			//cout << dp[i][j][2] << " ";
-		}//cout << endl;
-	}
-	cout << ans;
+
+	cout << max(dp[t][w][2], dp[t][w][1]);
 	return 0;
 
 }
