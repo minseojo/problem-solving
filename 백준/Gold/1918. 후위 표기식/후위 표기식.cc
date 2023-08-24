40a30bec7240fa00db8612040d18fc06c6a66b8a
Minseo  Jo <liging12@naver.com>
2023-08-24 22:46:59 +0900

    [Gold II] Title: 후위 표기식, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s, ans = "";
+	cin >> s;
+	stack<char> ex;
+	
+	for (int i = 0; i < s.size(); i++) {
+		if ('A' <= s[i] && s[i] <= 'Z') {
+			ans += s[i];	
+		}
+		else if (s[i] == '(') {
+			ex.push('(');
+		}
+		else if(s[i] == ')') {
+			while(!ex.empty() && ex.top() != '(') {
+				ans += ex.top();
+				ex.pop();
+			}
+			ex.pop();
+		}
+		else if (s[i] == '*' || s[i] == '/') {
+			while (!ex.empty() && (ex.top() == '*' || ex.top() == '/')) {
+				ans += ex.top();
+				ex.pop();
+			}
+			ex.push(s[i]);
+		} 
+		else if (s[i] == '+' || s[i] == '-') {
+			while (!ex.empty() && ex.top() != '(') {
+				ans += ex.top();
+				ex.pop();
+			}
+			ex.push(s[i]);
+		}
+	}
+	while(!ex.empty()) {
+		ans += ex.top();
+		ex.pop();
+	}
+	
+ 	cout << ans;
+	return 0;
+}
