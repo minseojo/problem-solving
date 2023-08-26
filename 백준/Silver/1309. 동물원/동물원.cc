4c137817c46082c40b94fd076859d2021dbec65d
Minseo  Jo <liging12@naver.com>
2023-08-26 14:25:17 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -11,9 +11,9 @@ int main() {
 	dp[1][0] = dp[1][1] = dp[1][2] = 1;
 	 
 	for (int i = 2; i <= N; i++) {
-		dp[i][0] = dp[i-1][0] % MOD + dp[i-1][1] % MOD + dp[i-1][2] % MOD;
-		dp[i][1] = dp[i-1][0] % MOD + dp[i-1][2] % MOD;
-		dp[i][2] = dp[i-1][0] % MOD + dp[i-1][1] % MOD;
+		dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
+		dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
+		dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
 	}
 	
 	int ans = 0;
