9cc258ef2cb509d0cc725c14c7c08ddb50c6482b
Minseo  Jo <liging12@naver.com>
2023-08-26 17:53:20 +0900

    [Silver I] Title: 기타리스트, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Silver/1495. 기타리스트/기타리스트.cc
+++ 백준/Silver/1495. 기타리스트/기타리스트.cc
@@ -1,64 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Point {
-	int volume; // 현재 볼륨 
-	int index; // 현재 인덱스 
-};
-int N, S, M;
-int a[50];
-bool visited[51][1001];
-bool isLastDance = true;
-int ans;
-
-void BFS() {
-	queue<Point> q;
-	q.push({S, 0});
-	visited[0][S] = true;
-	
-	while (!q.empty()) {
-		int size = q.size();
-		int dance_cnt = 0;
-		while (size--) {
-			int volume = q.front().volume;
-			int index = q.front().index;
-			int dif = a[index];
-			q.pop();
-			
-			if (index == N && !visited[index][volume]) {
-				visited[index][volume] = true;
-				dance_cnt++;
-				ans = max(ans, volume);
-				continue;
-			}
-
-			if (volume - dif >= 0 && !visited[index][volume - dif]) {
-				visited[index][volume - dif] = true;
-				q.push({volume - dif, index + 1});
-				dance_cnt++;
-			}
-			
-			if (volume + dif <= M && !visited[index][volume + dif]) {
-				visited[index][volume + dif] = true;
-				q.push({volume + dif, index + 1});
-				dance_cnt++;
-			}
-		}
-		if (dance_cnt == 0) {
-			isLastDance = false;
-			return;
-		}
-	}
-	
-}
+int a[51];
+bool dp[51][1001];
 
 int main() {
-	cin >> N >> S >> M;
-	for (int i = 0; i < N; i++) {
-		cin >> a[i];
-	}
-	BFS();
-	if (isLastDance) cout << ans << "\n";
-	else cout << -1 << "\n";
-	return 0;
+    int N, S, M;
+    cin >> N >> S >> M;
+    for (int i = 1; i <= N; i++) cin >> a[i];
+
+    if (S + a[1] <= M) dp[1][S + a[1]] = true;
+    if (S - a[1] >= 0) dp[1][S - a[1]] = true;   
+    for (int i = 2; i <= N; i++) {
+        int volume = a[i];
+        for (int j = 0; j <= M; j++) {
+            if (dp[i - 1][j]) {
+                if (j + volume<= M) dp[i][j + volume] = true;
+                if (j - volume >= 0) dp[i][j - volume] = true;
+            }
+        }
+    }
+
+    for (int i = M; i >= 0; i--) {
+        if (dp[N][i]) {
+            cout << i << "\n";
+            return 0;
+        }
+    }
+    
+    cout << -1 << "\n";
+    return 0;
 }
