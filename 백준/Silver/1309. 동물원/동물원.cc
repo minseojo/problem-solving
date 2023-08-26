507b52cd0250a79dd79c7206c1fc65556a1e8d22
Minseo  Jo <liging12@naver.com>
2023-08-26 14:24:42 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.cc
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -1,19 +1,25 @@
 #include <bits/stdc++.h>
+#define MAX 100001
+#define MOD 9901
 using namespace std;
 
-int dp[100001][3];
-const int MOD = 9901;
+int N;
+int dp[MAX][3]; // 안넣은거, 왼쪽, 오른쪽 
+
 int main() {
-	int n;
-	cin >> n;
-	dp[0][0] = 1;
-	dp[1][0] = 1; dp[1][1] = 1; dp[1][2] = 1;
-	
-	for(int i=2; i<=n; i++) {
-		dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % MOD;
-		dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD; 
-		dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;                                                                                                                                       
+	cin >> N;
+	dp[1][0] = dp[1][1] = dp[1][2] = 1;
+	 
+	for (int i = 2; i <= N; i++) {
+		dp[i][0] = dp[i-1][0] % MOD + dp[i-1][1] % MOD + dp[i-1][2] % MOD;
+		dp[i][1] = dp[i-1][0] % MOD + dp[i-1][2] % MOD;
+		dp[i][2] = dp[i-1][0] % MOD + dp[i-1][1] % MOD;
 	}
 	
-	cout << (dp[n][0] + dp[n][1] + dp[n][2]) % MOD;
+	int ans = 0;
+	for (int i = 0; i < 3; i++) {
+		ans += (dp[N][i] % MOD) ;
+	}
+	cout << ans % MOD;
+	return 0;
 }
