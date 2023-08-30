5c9231fa307a08549762064e9acf77da5f47bf4a
Minseo  Jo <liging12@naver.com>
2023-08-30 20:17:18 +0900

    [Gold II] Title: 도토리 숨기기, Time: 16 ms, Memory: 2136 KB -BaekjoonHub

--- 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.cc
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.cc
@@ -1,22 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long rule[10000][3];
+int rule[10000][3];
 
 int main() {
-    long long N, K, D;
+    int N, K;
+    long long D;
     cin >> N >> K >> D;
     for (int i = 0; i < K; i++) {
         cin >> rule[i][0] >> rule[i][1] >> rule[i][2];
     }
 
-    long long l = 1, r = N, ans = 1e6;
+    int l = 1, r = N, ans = 1e6;
     while (l <= r) {
-        long long m = (l + r) / 2;
+        int m = (l + r) / 2;
 
         long long d = 0;
         for (int i = 0; i < K; i++) {
-            long long x = min(m, rule[i][1]);
+            int x = min(m, rule[i][1]);
             if (rule[i][0] <= x) {
                 d = d + (x - rule[i][0]) / rule[i][2] + 1;
             }
