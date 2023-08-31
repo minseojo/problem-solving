fc24ace89c6fd262917c16ec0f9d8c5e570754e9
Minseo  Jo <liging12@naver.com>
2023-08-31 17:37:21 +0900

    [Gold V] Title: LCS, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -1,36 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string a, b;
-int dp[1001];
+int dp[1000];
 
-void solve() {
-    int cnt = 0;
-    for(int i=0; i<a.length(); i++) {
-        cnt = 0;
-        for(int j=0; j<b.length(); j++) {
-            if(cnt < dp[j]) cnt = dp[j];
-            else if(a[i] == b[j]) {
-                dp[j] = cnt+1;
+int main() {
+    string a, b;
+    cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        int cnt = 0;
+        for (int j = 0; j < b.size(); j++) {
+            if (cnt < dp[j]) cnt = dp[j];
+            else if (a[i] == b[j]) {
+                dp[j] = cnt + 1;
             }
         }
     }
-}
-
-int getAnswer() {
-    int answer = 0;
-    for(int i=0; i<b.length(); i++) {
-        if(answer < dp[i]) answer = dp[i];
+    int ans = 0;
+    for (int i = 0; i < b.size(); i++) {
+        ans = max(ans, dp[i]);
     }
-    
-    return answer;
-}
-
-int main() {
-	cin >> a >> b;
-    
-    solve();
-    cout << getAnswer();
-    
+    cout << ans;
     return 0;
 }
