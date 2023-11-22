74b1a2551e984cd6d40ce4895755bc923a3a01d8
minseo Jo <liging12@naver.com>
2023-11-22 20:31:41 +0900

    [level 2] Title: 주식가격, Time: 21.91 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,3 +1,29 @@
+// 시간 복잡도 : O(n)
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = prices.length - index - 1;
+        }
+
+        return answer;
+    }
+}
+
+/* 시간 복잡도: O(n^2)
 import java.util.*;
 
 class Solution {
@@ -22,3 +48,4 @@ class Solution {
             .toArray();
     }
 }
+*/
