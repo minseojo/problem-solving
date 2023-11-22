391b73b4ec6a3425b1e690facfda3ba248e49ad7
minseo Jo <liging12@naver.com>
2023-11-22 20:06:04 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.87 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -3,41 +3,41 @@ import java.util.*;
 class Solution {
     
     class Truck implements Comparable<Truck> {
-        int time;
         int weight;
+        int position;
         
-        Truck(int time, int weight) {
-            this.time = time;
+        Truck(int weight) {
             this.weight = weight;
+            this.position = 0;
         }
         
-        void setTime(int time) {
-            this.time = time;
+        void foward(int step) {
+            this.position += step;
         }
         
         @Override
         public int compareTo(Truck truck) {
-            return Integer.compare(this.time, truck.time);
+            return Integer.compare(this.position, truck.position);
         }
     }
     
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Truck> waitQueue = new LinkedList<>();
         for (int i = 0; i < truck_weights.length; i++) {
-            waitQueue.add(new Truck(0, truck_weights[i]));
+            waitQueue.add(new Truck(truck_weights[i]));
         }
         
-        int time = 1;
+        int time = 0;
         int curWeight = 0;
         PriorityQueue<Truck> runQueue = new PriorityQueue<>();
         
         while (!waitQueue.isEmpty() || !runQueue.isEmpty()) {               
             if (!runQueue.isEmpty()) {
                 Truck truck = runQueue.peek();
-                int initTime = truck.time;
+                int truckPosition = truck.position;
                 int truckWeight = truck.weight;
-                int elapsedTime = time - initTime;
-
+                
+                int elapsedTime = time - truckPosition;
                 if (elapsedTime >= bridge_length) {
                     curWeight -= truckWeight;
                     runQueue.poll();
@@ -46,13 +46,17 @@ class Solution {
             
             if (!waitQueue.isEmpty() && curWeight + waitQueue.peek().weight <= weight) {
                 Truck truck = waitQueue.poll();
-                truck.setTime(time);
                 runQueue.add(truck);
                 curWeight += truck.weight;
+            }
+            
+            for (Truck truck : waitQueue) {
+                int step = 1;
+                truck.foward(step);
             }
             time++;
         }
         
-        return time - 1;
+        return time;
     }
 }
