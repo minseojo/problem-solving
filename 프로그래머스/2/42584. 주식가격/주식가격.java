12e51b3cf768dd47f835975d17248996f996f7ee
minseo Jo <liging12@naver.com>
2023-11-23 14:25:00 +0900

    [level 2] Title: 주식가격, Time: 20.48 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,4 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int top = stack.pop();
+                answer[top] = i - top;
+            }
+            stack.add(i);
+        }
+        
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = prices.length - top - 1;
+        }
+
+        return answer;
+    }
+}
+
 // 시간 복잡도 : O(n)
+/*
 import java.util.*;
 
 class Solution {
@@ -8,44 +33,18 @@ class Solution {
 
         for (int i = 0; i < prices.length; i++) {
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+                int top = stack.pop();
+                answer[top] = i - top;
             }
             stack.push(i);
         }
 
         while (!stack.isEmpty()) {
-            int index = stack.pop();
-            answer[index] = prices.length - index - 1;
+            int top = stack.pop();
+            answer[top] = prices.length - top - 1;
         }
 
         return answer;
-    }
-}
-
-/* 시간 복잡도: O(n^2)
-import java.util.*;
-
-class Solution {
-    public int[] solution(int[] prices) {
-        int size = prices.length;
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < size; i++) {
-            int price = prices[i];
-            int time = 0;
-            for (int j = i + 1; j < size; j++) {
-                time++;
-                if (price > prices[j]) {
-                    break;
-                }
-            }
-            list.add(time);
-        }
-        
-        return list.stream()
-            .mapToInt(Integer::intValue)
-            .toArray();
     }
 }
 */
