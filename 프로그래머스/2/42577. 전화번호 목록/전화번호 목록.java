8464e362ec2e5befcd1e90cb82dd65f0c82dc42e
minseo Jo <liging12@naver.com>
2023-12-08 13:32:04 +0900

    [level 2] Title: 전화번호 목록, Time: 299.41 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -6,12 +6,7 @@ class Solution {
         for (int i = 0; i < phone_book.length - 1; i++) {
             int length = Math.min(phone_book[i].length(), phone_book[i + 1].length());
             
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < length; j++) {
-                sb.append(phone_book[i + 1].charAt(j));
-            }
-
-            if (phone_book[i].equals(sb.toString())) {
+            if (phone_book[i].startsWith(phone_book[i + 1].substring(0, length))) {
                 return false;
             }
         }
