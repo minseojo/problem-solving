19cd43d030e3ff45b1be71161fa8a49477197ed0
minseo Jo <liging12@naver.com>
2024-02-18 19:47:03 +0900

    [level 3] Title: N으로 표현, Time: 24.16 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
 class Solution {
-    int digitN;
     int answer;
     public int solution(int N, int number) {
         answer = Integer.MAX_VALUE;
         int[] x = new int[11];
         int tmpN = N;
-
+        int digitN = 0;
         while (tmpN > 0) {
             tmpN /= 10;
             digitN++;
@@ -19,24 +18,27 @@ class Solution {
             }
             x[i] = Integer.valueOf(tmp);
         }
-
         dp(N, number, 0, 0, x);
+
         if (answer > 8) return -1;
         return answer;
     }
     
     private void dp(int N, int number, int cur, int count, int[] x) {
-        if (count > 8 || (cur == 0 && count > 0)) {
+        if (count > 8 || count > 0 && cur == 0) {
             return;
         }
+        
         if (cur == number) {
             answer = Math.min(answer, count);
         }
-        for (int i = 1; i <= 9; i+= digitN) {
-            dp(N, number, cur + x[i], count + i, x);
-            dp(N, number, cur - x[i], count + i, x);
-            dp(N, number, cur * x[i], count + i, x);
+        
+        for (int i = 1; i <= 9; i++) {
+            dp(N, number, cur + x[i], count + i, x);  
+            dp(N, number, cur - x[i], count + i, x); 
+            dp(N, number, cur * x[i], count + i, x); 
             dp(N, number, cur / x[i], count + i, x);
         }
+
     }
 }
