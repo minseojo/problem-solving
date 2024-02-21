4005c5678894d0e39d5610d391c602e21e288a25
minseo Jo <liging12@naver.com>
2024-02-22 04:17:23 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,20 +4,20 @@ class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder(number);
         
-        int j = 0;
+        int pos = 0;
         for (int i = 0; i < k; i++) {
             while (true) {
-                if (j == sb.length() - 1) {
-                    sb.delete(j, j + 1);
+                if (pos == sb.length() - 1) {
+                    sb.delete(pos, pos + 1);
                     break;
                 }
-                if (sb.charAt(j) < sb.charAt(j + 1)) {
-                    sb.delete(j, j + 1);
+                if (sb.charAt(pos) < sb.charAt(pos + 1)) {
+                    sb.delete(pos, pos + 1);
                     break;
                 }
-                j++;
+                pos++;
             }
-            if (j > 0) j--;
+            if (pos > 0) pos--;
         }
         
         return sb.toString();
