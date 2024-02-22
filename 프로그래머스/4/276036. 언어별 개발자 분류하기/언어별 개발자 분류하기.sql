3c70800368a71cf70e0817eee3ee0b86134584a2
minseo Jo <liging12@naver.com>
2024-02-22 17:48:29 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,26 +1,47 @@
-SELECT 
-    (CASE 
-        WHEN EXISTS (SELECT S.CODE 
-                FROM SKILLCODES S 
-                WHERE S.CODE & D.SKILL_CODE AND 
-                S.NAME = 'Python') AND 
-            EXISTS (SELECT S.CODE 
-                FROM SKILLCODES S 
-                WHERE S.CODE & D.SKILL_CODE AND 
-                S.NAME != 'Python' AND # 파이썬이 아닌 경우, 실제로는 파이썬은 Back End라 안겹침
-                S.CATEGORY = 'Front End') THEN 'A'
+# SELECT 
+#     (CASE 
+#         WHEN EXISTS (SELECT S.CODE 
+#                 FROM SKILLCODES S 
+#                 WHERE S.CODE & D.SKILL_CODE AND 
+#                 S.NAME = 'Python') AND 
+#             EXISTS (SELECT S.CODE 
+#                 FROM SKILLCODES S 
+#                 WHERE S.CODE & D.SKILL_CODE AND 
+#                 S.NAME != 'Python' AND # 파이썬이 아닌 경우, 실제로는 파이썬은 Back End라 안겹침
+#                 S.CATEGORY = 'Front End') THEN 'A'
 
-        WHEN EXISTS (SELECT S.CODE 
-                FROM SKILLCODES S 
-                WHERE S.CODE & D.SKILL_CODE AND 
-                S.NAME = 'C#') THEN 'B'
+#         WHEN EXISTS (SELECT S.CODE 
+#                 FROM SKILLCODES S 
+#                 WHERE S.CODE & D.SKILL_CODE AND 
+#                 S.NAME = 'C#') THEN 'B'
 
-        WHEN EXISTS (SELECT S.CODE 
-                FROM SKILLCODES S 
-                WHERE S.CODE & D.SKILL_CODE AND 
-                S.CATEGORY = 'Front End') THEN 'C'
-    ELSE NULL
-    END) AS GRADE, D.ID, D.EMAIL
-FROM DEVELOPERS D
-HAVING GRADE IS NOT NULL
-ORDER BY GRADE, ID
+#         WHEN EXISTS (SELECT S.CODE 
+#                 FROM SKILLCODES S 
+#                 WHERE S.CODE & D.SKILL_CODE AND 
+#                 S.CATEGORY = 'Front End') THEN 'C'
+#     ELSE NULL
+#     END) AS GRADE, D.ID, D.EMAIL
+# FROM DEVELOPERS D
+# HAVING GRADE IS NOT NULL
+# ORDER BY GRADE, ID
+
+WITH FRONT AS (SELECT BIT_OR(CODE) AS FBIT
+              FROM SKILLCODES
+              WHERE CATEGORY = 'Front End')
+SELECT *
+FROM (SELECT 
+        CASE  
+            WHEN SKILL_CODE & (SELECT CODE 
+                               FROM SKILLCODES 
+                               WHERE NAME = 'C#')
+                THEN 'B'
+            WHEN SKILL_CODE & (SELECT FBIT FROM FRONT)
+                THEN IF(SKILL_CODE & (SELECT CODE 
+                                      FROM SKILLCODES 
+                                      WHERE NAME = 'Python'), 'A', 'C')
+            ELSE NULL
+        END AS GRADE,
+        ID, EMAIL
+    FROM DEVELOPERS) T
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID;
