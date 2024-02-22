f5883478564653684bfea1ec61da30d60eb5bd00
minseo Jo <liging12@naver.com>
2024-02-22 20:56:03 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,21 +1,24 @@
 -- 코드를 작성해주세요
+
 with front_end_skill as (
     select *
     from skillcodes
     where category = 'Front End'
 )
 
-# select distinct(id), email, first_name, last_name
-# from developers as d
-# where exists (
-#     select *
-#     from front_end_skill fk
-#     where d.skill_code & fk.code
-# )
-# order by id
-
+#exists 활용
 select distinct(id), email, first_name, last_name
 from developers as d
-cross join front_end_skill fk
-where d.skill_code & fk.code && fk.category = 'Front End'
+where exists (
+    select *
+    from front_end_skill fk
+    where d.skill_code & fk.code
+)
 order by id
+
+# # join 활용
+# select distinct(id), email, first_name, last_name
+# from developers as d
+# cross join front_end_skill fk
+# where d.skill_code & fk.code && fk.category = 'Front End'
+# order by id;
