1b8041d0d106fab372d86302848b612428d7ceab
minseo Jo <liging12@naver.com>
2024-02-29 18:10:57 +0900

    [level 1] Title: 옹알이 (2), Time: 0.61 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -2,25 +2,16 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         for (String b : babbling) {
-            b = b.replace("aya", "1");
-            b = b.replace("ye", "2");
-            b = b.replace("woo", "3");
-            b = b.replace("ma", "4");
+            if (b.contains("ayaaya") || b.contains("yeye") || 
+                b.contains("woowoo") || b.contains("mama")) continue;
             
-            boolean ok = true;
-            for (int i = 0; i < b.length(); i++) {
-                if (!('1' <= b.charAt(i) && b.charAt(i) <= '4')) {
-                    ok = false;
-                    break;
-                }
-                
-                if (i < b.length() - 1 && b.charAt(i) == b.charAt(i+1)) {
-                    ok = false;
-                    break;
-                }
-            }
-            if (ok) answer++;
+            b = b.replace("aya", " ");
+            b = b.replace("ye", " ");
+            b = b.replace("woo", " ");
+            b = b.replace("ma", " ");
+            b = b.replace(" ", "");
             
+            if (b.isEmpty()) answer++;
         }
         return answer;
     }
