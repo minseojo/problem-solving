a6fe71c75a7a8cce7b4a9b02c28134263f5c450e
minseo Jo <liging12@naver.com>
2024-05-25 03:40:59 +0900

    [Silver II] Title: 화분 부수기, Time: 496 ms, Memory: 98208 KB -BaekjoonHub

---
+++ 백준/Silver/3088. 화분 부수기/화분 부수기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int result = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[1000001];
+
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (arr[a] == 0 && arr[b] == 0 && arr[c] == 0) {
+                result++;
+            }
+            arr[a]++;
+            arr[b]++;
+            arr[c]++;
+        }
+
+        System.out.println(result);
+    }
+
+}
