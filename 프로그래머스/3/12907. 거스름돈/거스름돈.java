b11a2cd7471d1177d1e80c4126b7ab38d352c5b8
minseo Jo <liging12@naver.com>
2024-06-21 22:38:27 +0900

    [level 3] Title: 거스름돈, Time: 12.24 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,16 +1,18 @@
+import java.util.*;
+
 class Solution {
 
     static final int MOD = 1_000_000_007;
     
     public int solution(int n, int[] money) {
         int[] sum = new int[n + 1];
+        sum[0] = 1; 
+        
+        Arrays.sort(money);
         
         for (int m : money) {
-            sum[m]++;
-            for (int j = 1; j <= n; j++) {
-                if (j - m > 0) {
-                    sum[j] = (sum[j] + sum[j - m]) % MOD;
-                }
+            for (int j = m; j <= n; j++) {
+                sum[j] = (sum[j] + sum[j - m]) % MOD;
             }
         }
         
