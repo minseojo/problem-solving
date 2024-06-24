c74f08a349bff21f5b6a4feb27d80e76e3348889
minseo Jo <liging12@naver.com>
2024-06-24 23:39:12 +0900

    Time: 15 ms (26.33%), Space: 45 MB (22.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,25 +1,30 @@
 class Solution {
+    
     public boolean isPalindrome(String s) {
-
-        StringBuilder temp = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                temp.append(Character.toLowerCase(c));
-            }
-        }
-
-        int n = temp.length();
-        if (n == 0 || n == 1) {
-            return true;
-        }
-        
-        for (int i = 0; i < n / 2; i++) {
-            if (temp.charAt(i) != temp.charAt(n - i - 1)) {
-                return false;
-            }
-        }
-        
-        return true;
+        String s_filterd = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        String s_reversed = new StringBuilder(s_filterd).reverse().toString();
+        return s_filterd.equals(s_reversed);
     }
+    
+//     public boolean isPalindrome(String s) {
+//         int start = 0;
+//         int end = s.length() - 1;
+        
+//         while (start < end) {
+//             if (!Character.isLetterOrDigit(s.charAt(start))) {
+//                 start++;
+//             } else if(!Character.isLetterOrDigit(s.charAt(end))) {
+//                 end--;
+//             } else {
+//                 if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+//                     return false;
+//                 }
+                
+//                 start++;
+//                 end--;
+//             }
+//         }
+        
+//         return true;
+//     }
 }
