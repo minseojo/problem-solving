87624f94d9c1771fdfd7349c8f5cfff008b1b718
minseo Jo <liging12@naver.com>
2024-06-25 15:45:31 +0900

    Time: 4 ms (71.33%), Space: 44.5 MB (78.9%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -4,7 +4,7 @@ class Solution {
     public String[] reorderLogFiles(String[] logs) {
         List<String> letterLogs = new ArrayList<>();
         List<String> digitLogs = new ArrayList<>();
-        
+
         for (String log : logs) {
             if (Character.isDigit(log.split(" ")[1].charAt(0))) {
                 digitLogs.add(log);
@@ -12,11 +12,11 @@ class Solution {
                 letterLogs.add(log);
             }
         }
-        
+
         letterLogs.sort((s1, s2) -> {
             String[] s1Log = s1.split(" ", 2);
             String[] s2Log = s2.split(" ", 2);
-            
+
             // 비교 대상의 순서가 동일한 경우 0, 순서가 앞인 경우 1, 순서가 뒤인 경우 -1
             int compared = s1Log[1].compareTo(s2Log[1]);
             // 로그가 동일한 경우 식별자 비교
@@ -26,9 +26,9 @@ class Solution {
                 return compared;
             }
         });
-            
+
         letterLogs.addAll(digitLogs);
-        
+
         return letterLogs.toArray(new String[0]);
     }
 }
