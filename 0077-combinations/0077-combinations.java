43d12e789958658160a1dce0ae538d01cc1f07bf
minseo Jo <liging12@naver.com>
2024-07-08 20:18:13 +0900

    Time: 18 ms (75.53%), Space: 94.5 MB (49.02%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> results = new ArrayList<>();
-        dfs(0, 1, new ArrayList<>(), new boolean[n + 1], n, k, results);
+        dfs(0, 1, new ArrayList<>(), n, k, results);
         return results;
     }
 
-    void dfs(int pos, int index, List<Integer> cache, boolean[] isCache, int n, int k, List<List<Integer>> results) {
+    void dfs(int pos, int index, List<Integer> cache, int n, int k, List<List<Integer>> results) {
         if (pos == k) {
             results.add(new ArrayList<>(cache));
             return;
         }
 
         for (int i = index;  i <= n; i++) {
-            if (isCache[i]) continue;
-            isCache[i] = true;
             cache.add(i);
-            dfs(pos + 1, i + 1, cache, isCache, n, k, results);
+            dfs(pos + 1, i + 1, cache, n, k, results);
             cache.remove(cache.size() - 1);
-            isCache[i] = false;
         }
     }
 }
