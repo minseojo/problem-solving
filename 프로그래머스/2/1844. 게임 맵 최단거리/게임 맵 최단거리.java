b2112d1e8193c08a5e5bb250461d857de67d6069
minseo Jo <liging12@naver.com>
2024-07-13 03:32:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.84 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,42 @@
-/*
-시작: 0, 0
-끝: n-1, m-1
-*/
 import java.util.*;
 
 class Solution {
-    static int[] dy = {0, 0, 1, -1};
-    static int[] dx = {1, -1, 0, 0};
-    
-    class Position {
-        public int y;
-        public int x;
-        
+    static final int[] dy = {0, 0, 1, -1};
+    static final int[] dx = {1, -1, 0, 0};
+
+    static class Position {
+        int y;
+        int x;
+
         Position(int y, int x) {
             this.y = y;
             this.x = x;
         }
     }
-    
+
     public int solution(int[][] maps) {
         int answer = 0;
         int n = maps.length;
         int m = maps[0].length;
-        Queue<Position> q = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        
+        Deque<Position> q = new ArrayDeque<>();
         q.add(new Position(0, 0));
+        boolean[][] visited = new boolean[n][m];
         visited[0][0] = true;
+
         while (!q.isEmpty()) {
-            int size = q.size();
             answer++;
+            int size = q.size();
             while (size-- > 0) {
-                Position p = q.poll();
-                int y = p.y;
-                int x = p.x;
+                Position cur = q.poll();
+                int y = cur.y;
+                int x = cur.x;
                 if (y == n - 1 && x == m - 1) {
                     return answer;
                 }
-                
-                for (int dir = 0; dir < 4; dir++) {
-                    int ny = y + dy[dir];
-                    int nx = x + dx[dir];
-                    if (ny < 0 || nx < 0 || ny > n - 1 || nx > m - 1) {
-                        continue;
-                    }
+                for (int i = 0; i < 4; i++) {
+                    int ny = y + dy[i];
+                    int nx = x + dx[i];
+                    if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
                     if (maps[ny][nx] == 1 && !visited[ny][nx]) {
                         visited[ny][nx] = true;
                         q.add(new Position(ny, nx));
@@ -51,7 +44,7 @@ class Solution {
                 }
             }
         }
-        
+
         return -1;
     }
 }
