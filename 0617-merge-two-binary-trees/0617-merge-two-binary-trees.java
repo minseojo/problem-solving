5679d00b75d65dcde7db53f2e0e0ef3891f02be8
minseo Jo <liging12@naver.com>
2024-07-16 21:42:49 +0900

    Time: 0 ms (100%), Space: 45.2 MB (5.61%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -15,48 +15,12 @@
  */
 class Solution {
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) return null;
-        TreeNode temp = new TreeNode();
-        TreeNode root = temp;
-        dfs(temp, root1, root2);
-        return root;
-    }
-
-    TreeNode dfs(TreeNode root, TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) return null;
-
-        if (root1 == null) {
-            root.val = root2.val;
-            if (root2.left != null) {
-                root.left = new TreeNode();
-                dfs(root.left, null, root2.left);
-            }
-            if (root2.right != null) {
-                root.right = new TreeNode();
-                dfs(root.right, null, root2.right);
-            }
-        } else if (root2 == null) {
-            root.val = root1.val;
-            if (root1.left != null) {
-                root.left = new TreeNode();
-                dfs(root.left, root1.left, null);
-            }
-            if (root1.right != null) {
-                root.right = new TreeNode();
-                dfs(root.right, root1.right, null);
-            }
-        } else {
-            root.val = root1.val + root2.val;
-            if (root1.left != null || root2.left != null) {
-                root.left = new TreeNode();
-                dfs(root.left, root1.left, root2.left);
-            }
-            if (root1.right != null || root2.right != null) {
-                root.right = new TreeNode();
-                dfs(root.right, root1.right, root2.right);
-            }
-        }
-
-        return root;
+        if (root1 == null) return root2;
+        if (root2 == null) return root1;
+        
+        TreeNode node = new TreeNode(root1.val + root2.val);
+        node.left = mergeTrees(root1.left, root2.left);
+        node.right = mergeTrees(root1.right, root2.right);
+        return node;
     }
 }
