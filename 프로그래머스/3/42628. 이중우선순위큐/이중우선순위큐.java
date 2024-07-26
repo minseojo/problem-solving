9250a657cb10cdc93bed87935601bebbaf3d0706
minseo Jo <liging12@naver.com>
2024-07-26 20:59:47 +0900

    [level 3] Title: 이중우선순위큐, Time: 73.14 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,40 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        int count = 0;
-
-        for (String operation : operations) {
+        Queue<Integer> minHeap = new PriorityQueue<>();
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (String operation: operations) {
             String[] op = operation.split(" ");
             if (op[0].equals("I")) {
-                int num = Integer.parseInt(op[1]);
-                map.put(num, map.getOrDefault(num, 0) + 1);
-                count++;
+                minHeap.add(Integer.valueOf(op[1]));
+                maxHeap.add(Integer.valueOf(op[1]));
             } else {
-                if (count == 0) continue;
                 if (op[1].equals("1")) {
-                    int maxKey = map.lastKey();
-                    if (map.get(maxKey) == 1) {
-                        map.remove(maxKey);
-                    } else {
-                        map.put(maxKey, map.get(maxKey) - 1);
-                    }
+                    minHeap.remove(maxHeap.poll());
                 } else if (op[1].equals("-1")) {
-                    int minKey = map.firstKey();
-                    if (map.get(minKey) == 1) {
-                        map.remove(minKey);
-                    } else {
-                        map.put(minKey, map.get(minKey) - 1);
-                    }
+                    maxHeap.remove(minHeap.poll());
                 }
-                count--;
             }
         }
-
-        if (count == 0) {
-            return new int[]{0, 0};
-        } else {
-            return new int[]{map.lastKey(), map.firstKey()};
-        }
+        
+        return new int[]{maxHeap.isEmpty() ? 0 : maxHeap.peek(), 
+                         minHeap.isEmpty() ? 0 : minHeap.peek()};
     }
 }
