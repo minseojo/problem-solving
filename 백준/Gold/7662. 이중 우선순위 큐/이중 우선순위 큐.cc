c4ae87667b67c452ae044a0b72e9bb26b883705b
minseo Jo <liging12@naver.com>
2024-07-26 20:56:10 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2656 ms, Memory: 48884 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int tc;
+	cin >> tc;
+
+	while(tc--) {
+		multiset<int> s;
+		int n, x;
+		char c;
+		cin >> n;
+		for(int i=0; i<n; i++) {
+			cin >> c >> x;
+			
+			if(c=='I') {
+				s.insert(x);
+			} else {
+				if(s.empty()) continue;
+				else if(x==1) {
+					auto iter = s.end();
+					iter--;
+					s.erase(iter);
+				} else {
+					auto iter = s.begin();
+					s.erase(iter);
+				}
+			}
+		}
+		if(!s.empty()) {
+			auto end=s.end();
+			end--;
+			cout << *end << " " << *s.begin() << endl;
+		}
+		else cout <<"EMPTY" << endl;
+	}
+} 
