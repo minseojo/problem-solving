9dc3dd279780e70e59151881689478127bcb07c4
minseo Jo <liging12@naver.com>
2024-08-10 22:53:31 +0900

    Time: 4 ms (76.11%), Space: 44.8 MB (50.1%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.java
@@ -1,34 +1,30 @@
-import java.util.*;
-
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        List<String> letterLogs = new ArrayList<>();
+        List<String> wordLogs = new ArrayList<>();
         List<String> digitLogs = new ArrayList<>();
 
         for (String log : logs) {
             if (Character.isDigit(log.split(" ")[1].charAt(0))) {
                 digitLogs.add(log);
             } else {
-                letterLogs.add(log);
+                wordLogs.add(log);
             }
         }
 
-        letterLogs.sort((s1, s2) -> {
-            String[] s1Log = s1.split(" ", 2);
-            String[] s2Log = s2.split(" ", 2);
+        wordLogs.sort((l1, l2) -> {
+            String[] l1Split = l1.split(" ", 2);
+            String[] l2Split = l2.split(" ", 2);
 
-            // 비교 대상의 순서가 동일한 경우 0, 순서가 앞인 경우 1, 순서가 뒤인 경우 -1
-            int compared = s1Log[1].compareTo(s2Log[1]);
-            // 로그가 동일한 경우 식별자 비교
+            int compared = l1Split[1].compareTo(l2Split[1]);
             if (compared == 0) {
-                return s1Log[0].compareTo(s2Log[0]);
+                return l1Split[0].compareTo(l2Split[0]);
             } else {
                 return compared;
             }
         });
 
-        letterLogs.addAll(digitLogs);
+        wordLogs.addAll(digitLogs);
 
-        return letterLogs.toArray(new String[0]);
+        return wordLogs.toArray(new String[0]);
     }
 }
