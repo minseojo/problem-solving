8c73d73d50718051863a9cf978667d75ae7b548d
minseo Jo <liging12@naver.com>
2024-09-20 18:56:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.00 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 96.9 MB, 시간: 82.54 ms
+메모리: 94.9 MB, 시간: 86.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 05일 22:41:25
+2024년 09월 20일 18:56:44
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,17 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
+        Map<String, Integer> participants = new HashMap<>();
+        for (String p : participant) {
+            participants.put(p, participants.getOrDefault(p, 0) + 1);
         }
         
-        for (String name : completion) {
-            map.put(name, map.getOrDefault(name, 0) - 1);
-            if (map.get(name) == 0) map.remove(name);
+        for (String c : completion) {
+            if (participants.getOrDefault(c, 0) > 0) {
+                participants.put(c, participants.get(c) - 1);
+            }
         }
         
-        return map.entrySet().iterator().next().getKey();
+        for (String p : participant) {
+            if (participants.getOrDefault(p, 0) > 0) {
+                return p;
+            }
+        }
+        
+        return "";
     }
 }
