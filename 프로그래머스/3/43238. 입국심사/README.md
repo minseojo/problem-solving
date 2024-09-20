03d944995a7db238e1416564388a5ffd319f8f25
minseo Jo <liging12@naver.com>
2024-09-20 21:44:47 +0900

    [level 3] Title: 입국심사, Time: 63.30 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
 
 ### 성능 요약
 
-메모리: 67 MB, 시간: 0.07 ms
+메모리: 83.1 MB, 시간: 63.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 18:31:56
+2024년 09월 20일 21:44:45
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,24 +2,27 @@ class Solution {
     public long solution(int n, int[] times) {
         long answer = Long.MAX_VALUE;
         long l = 1, r = -1;
+        
         for (int t : times) {
             r = Math.max(r, t);
         }
         r *= n;
+        
         while (l <= r) {
             long m = (l + r) / 2;
-            long sum = 0;
-            for (long t : times) {
-                sum += (m / t);
+            long x = 0;
+            for (int t : times) {
+                x += (m / t);
             }
-
-            if (sum >= n) {
+            
+            if (x >= n) {
                 answer = Math.min(answer, m);
                 r = m - 1;
             } else {
                 l = m + 1;
             }
         }
+        
         return answer;
     }
 }
