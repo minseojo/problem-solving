c7cd3da37c8629c0da16ce9a26785429d08a5ae2
minseo Jo <liging12@naver.com>
2024-09-22 19:25:03 +0900

    [level 3] Title: 길 찾기 게임, Time: 34.46 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -2,107 +2,69 @@ import java.util.*;
 
 class Solution {
     
-    class Position {
-        int value;
-        int y;
-        int x;
-        
-        Position(int value, int x, int y) {
-            this.value = value;
-            this.y = y;
-            this.x = x;
-        }
-    }
+    int count;
     
     class Node {
-        int value;
-        int x;
+        int id;
         int y;
+        int x;
         Node left;
         Node right;
         
-        Node(){}
-        
-        Node (int value, int x, int y) {
-            this.value = value;
-            this.x = x;
+        Node (int id, int y, int x) {
+            this.id = id;
             this.y = y;
-        }
-        
-        void setValue(int value) {
-            this.value = value;
-        }
-        
-        void setChild(Node left, Node right) {
-            this.left = left;
-            this.right = right;
+            this.x = x;
         }
     }
     
     public int[][] solution(int[][] nodeinfo) {
-        List<Position> nodes = new ArrayList<>();
-        for (int i = 0; i < nodeinfo.length; i++) {
-            nodes.add(new Position(i+1, nodeinfo[i][0], nodeinfo[i][1]));
-        }
+        int[][] answer = new int[2][nodeinfo.length];
         
+        List<Node> nodes = new ArrayList<>();
+        for (int i = 0; i < nodeinfo.length; i++) {
+            nodes.add(new Node(i + 1, nodeinfo[i][1], nodeinfo[i][0]));
+        }
         Collections.sort(nodes, (i, j) -> {
-            if (i.y == j.y) {
-                return Integer.compare(i.x, j.x);
-            }
+            if (i.y == j.y) return Integer.compare(i.x, j.x);
             return Integer.compare(j.y, i.y);
         });
         
-        
-        Node root = new Node(nodes.get(0).value, nodes.get(0).x, nodes.get(0).y);
-        
-        for (int i = 1; i < nodes.size(); i++) {
-            setTree(root, new Node(nodes.get(i).value, nodes.get(i).x, nodes.get(i).y));
+        Node root = null;        
+        for (Node newNode : nodes) {
+            root = insertNode(root, newNode);
         }
 
-        int[][] answer = new int[2][nodeinfo.length]; // pre, post
         preOrder(root, answer);
         count = 0;
         postOrder(root, answer);
         
         return answer;
     }
-            static int count = 0;
     
-    private void setTree(Node root, Node child) {
-        if (root.x > child.x) {
-            if (root.left != null) {
-                setTree(root.left, child);
-            } else {
-                root.left = child;
-            }
-        }
-        
-        if (root.x < child.x) {
-            if (root.right != null) {
-                setTree(root.right, child);
-            } else {
-                root.right = child;
-            }
-        }
+    void preOrder(Node root, int[][] answer) {
+        if (root == null) return;
+        answer[0][count++] = root.id;
+        preOrder(root.left, answer);
+        preOrder(root.right, answer);
+    } 
+    
+    void postOrder(Node root, int[][] answer) {
+        if (root == null) return;
+        postOrder(root.left, answer);
+        postOrder(root.right, answer);
+        answer[1][count++] = root.id;
     }
     
-    private void preOrder (Node root, int[][] answer) {
-        answer[0][count++] = root.value;
-        if (root.left != null) {
-            preOrder(root.left, answer);
+    Node insertNode(Node root, Node newNode) {
+        if (root == null) return newNode;
+
+        if (root.x > newNode.x) {
+            root.left = insertNode(root.left, newNode);
+        } else if (root.x < newNode.x) {
+            root.right = insertNode(root.right, newNode);
         }
-        if (root.right != null) {
-            preOrder(root.right, answer);
-        }
-    }
-    
-    private void postOrder (Node root, int[][] answer) {
-        if (root.left != null) {
-            postOrder(root.left, answer);
-        }
-        if (root.right != null) {
-            postOrder(root.right, answer);
-        }
-        answer[1][count++] = root.value;
+
+        return root;
     }
 }
