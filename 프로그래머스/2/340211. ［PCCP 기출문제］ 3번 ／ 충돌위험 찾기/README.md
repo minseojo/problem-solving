ab61805795c90933da20c994cc6b2aeed3c2e28d
minseo Jo <liging12@naver.com>
2024-09-22 22:14:14 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 567.43 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 3번 / 충돌위험 찾기 - 340211 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211) 
 
 ### 성능 요약
 
-메모리: 208 MB, 시간: 533.64 ms
+메모리: 223 MB, 시간: 567.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 22일 21:55:11
+2024년 09월 22일 22:14:12
 
 ### 문제 설명
 
--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,75 +1,53 @@
 import java.util.*;
 
 class Solution {
-    class Point {
-        int r, c;
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-    }
-
     public int solution(int[][] points, int[][] routes) {
-        Map<Integer, Point> pointCoords = new HashMap<>();
-        for (int i = 0; i < points.length; i++) {
-            pointCoords.put(i + 1, new Point(points[i][0], points[i][1]));
-        }
+        int answer = 0;
+        Map<String, Integer> route = new HashMap<>();
+        
+        for (int[] r : routes) {
+            int time = 0;
+            int sr = points[r[0] - 1][0];
+            int sc = points[r[0] - 1][1];
 
-        int dangerCount = 0;
+            String key = time + "," + sr + ","+ sc;
 
-        // 특정 시간에 로봇이 위치한 좌표를 저장하는 맵
-        Map<String, Integer> robotPositions = new HashMap<>();
+            route.put(key, route.getOrDefault(key, 0) + 1);
 
-        // 각 로봇의 경로를 시뮬레이션
-        for (int robotId = 0; robotId < routes.length; robotId++) {
-            int currentTime = 0;
-
-            // 로봇의 경로 시작 위치 설정
-            Point startPoint = pointCoords.get(routes[robotId][0]);
-            int sr = startPoint.r, sc = startPoint.c;
-
-            // 첫 시작 위치 기록
-            String key = sr + "," + sc + "," + currentTime;
-            robotPositions.put(key, robotPositions.getOrDefault(key, 0) + 1);
-
-            // 로봇이 각 포인트를 순서대로 이동
-            for (int i = 1; i < routes[robotId].length; i++) {
-                Point endPoint = pointCoords.get(routes[robotId][i]);
-                int er = endPoint.r, ec = endPoint.c;
-
-                // r 좌표부터 이동
+            for (int i = 1; i < r.length; i++) {
+                int er = points[r[i] - 1][0];
+                int ec = points[r[i] - 1][1];
+ 
                 while (sr != er) {
                     if (sr < er) {
                         sr++;
                     } else {
                         sr--;
                     }
-                    currentTime++;
-                    key = sr + "," + sc + "," + currentTime;
-                    robotPositions.put(key, robotPositions.getOrDefault(key, 0) + 1);
+                    ++time;
+                    key = time + "," + sr + ","+ sc;
+                    route.put(key, route.getOrDefault(key, 0) + 1);
                 }
 
-                // c 좌표로 이동
                 while (sc != ec) {
                     if (sc < ec) {
                         sc++;
                     } else {
                         sc--;
                     }
-                    currentTime++;
-                    key = sr + "," + sc + "," + currentTime;
-                    robotPositions.put(key, robotPositions.getOrDefault(key, 0) + 1);
+                    ++time;
+                    key = time + "," + sr + ","+ sc;
+                    route.put(key, route.getOrDefault(key, 0) + 1);
                 }
             }
         }
-
-        // 충돌 상황 확인
-        for (int count : robotPositions.values()) {
+        
+        for (int count : route.values()) {
             if (count > 1) {
-                dangerCount++;
+                answer++;
             }
         }
-
-        return dangerCount;
+        
+        return answer;
     }
 }
