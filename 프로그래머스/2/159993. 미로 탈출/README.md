e6f01bc3a7fb547c0f7e5ffcc9a2c09bd69f69ac
minseo Jo <liging12@naver.com>
2024-09-23 19:57:31 +0900

    [level 2] Title: 미로 탈출, Time: 10.30 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=java) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.34 ms
+메모리: 80.6 MB, 시간: 10.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 13:32:40
+2024년 09월 23일 19:57:28
 
 ### 문제 설명
 
--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,80 +1,72 @@
 import java.util.*;
 
 class Solution {
+    
     static int[] dy = {1, -1, 0, 0};
     static int[] dx = {0, 0, 1, -1};
     
     class Point {
         int y;
         int x;
-        boolean hasKey;
-        Point(int y, int x, boolean hasKey) {
+        int hasLever;
+        
+        Point(int y, int x, int hasLever) {
             this.y = y;
             this.x = x;
-            this.hasKey = hasKey;
+            this.hasLever = hasLever;
         }
     }
     
     public int solution(String[] maps) {
-        int answer = bfs(maps);
-        return answer;
-    }
-    
-    private int bfs(String[] maps) {
-        int result = 0;
+        int answer = 0;
+        int[][][] map = new int[2][maps.length][maps[0].length()];
+                
         Queue<Point> q = new LinkedList<>();
-        int[][][] visited = new int[2][maps.length][maps[0].length()];
         
+        int ey = 0, ex = 0;
         for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length(); j++) {
-                if (maps[i].charAt(j) == 'S') {
-                    q.add(new Point(i, j, false));
-                    visited[0][i][j] = 1;
+            for (int j = 0; j < maps[0].length(); j++) {
+                char c = maps[i].charAt(j);
+                if (c == 'S') {
+                    q.add(new Point(i, j, 0));
+                    map[0][i][j] = 1;
+                }
+                if (c == 'E') {
+                    ey = i;
+                    ex = j;
                 }
             }
         }
-        
+
         while (!q.isEmpty()) {
             int size = q.size();
-            result++;
             while (size-- > 0) {
                 Point p = q.poll();
                 int y = p.y;
                 int x = p.x;
-                boolean hasKey = p.hasKey;
+                int hasLever = p.hasLever;
+                map[hasLever][y][x] = 1;
+                if (y == ey && x == ex && hasLever == 1) return answer;
+
                 for (int i = 0; i < 4; i++) {
                     int ny = y + dy[i];
                     int nx = x + dx[i];
-                    if (ny < 0 || nx < 0 || ny > maps.length - 1 || nx > maps[0].length() - 1) {
-                        continue;
-                    }
+                    if (ny < 0 || nx < 0 || ny >= maps.length || nx >= maps[0].length()) continue;
+                    if (map[hasLever][ny][nx] == 1 || maps[ny].charAt(nx) == 'X') continue;
                     
-                    if (!hasKey && visited[0][ny][nx] == 0 && maps[ny].charAt(nx) != 'X') {
-                        visited[0][ny][nx] = result;
-                        if (maps[ny].charAt(nx) == 'L') {
-                            q.add(new Point(ny, nx, true));
-                        } else {
-                            q.add(new Point(ny, nx, hasKey));
-                        }
+                    if (maps[ny].charAt(nx) == 'L') {
+                        map[1][ny][nx] = 1;
+                        q.add(new Point(ny, nx, 1));
                     }
-                    if (hasKey && visited[1][ny][nx] == 0 && maps[ny].charAt(nx) != 'X') {
-                        visited[1][ny][nx] = result;
-                        q.add(new Point(ny, nx, hasKey));
+                    else {
+                        q.add(new Point(ny, nx, hasLever));
+                        map[hasLever][ny][nx] = 1;
                     }
                 }
             }
+            answer++;
         }
         
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[i].length(); j++) {
-                if (maps[i].charAt(j) == 'E') {
-                    if (visited[1][i][j] != 0) {
-                        return visited[1][i][j];
-                    }
-                }
-            }
-        }
         return -1;
     }
-    
 }
