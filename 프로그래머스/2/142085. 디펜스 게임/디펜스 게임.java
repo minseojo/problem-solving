762415c8011f231852eeddf945365cee23d018de
minseo Jo <liging12@naver.com>
2024-10-23 03:35:09 +0900

    [level 2] Title: 디펜스 게임, Time: 117.17 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,28 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
-        int round = enemy.length;
-        int l = 0, r = round;
-        
-        while (l <= r) {
-            int m = (l + r) / 2;
-            long sum = 0;
-
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-            for (int i = 0 ; i < m; i++) {
-                pq.add(enemy[i]);
-                if (pq.size() > k) {
-                   sum += pq.poll();     
-                }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long sum = 0;
+        int kCount = 0;
+        for (int e : enemy) {
+            pq.add(e);
+            sum += e;
+            while (sum > n) {
+                if (!pq.isEmpty() && kCount < k) {
+                    sum -= pq.poll();
+                    kCount++;
+                } else break;
             }
-           
-            if (sum > n) {
-                r = m - 1;
-            } else {
-                answer = Math.max(answer, m);
-                l = m + 1;
-            }
+            
+            if (sum > n) break;
+            answer++;
         }
+
         return answer;
     }
 }
