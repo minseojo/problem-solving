42c22f5ef12584e1420b486d65e4ba1a305ba5b2
minseo Jo <liging12@naver.com>
2024-11-07 10:47:44 +0900

    [D2] Title: 달팽이 숫자, Time: 124 ms, Memory: 18,380 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -2,28 +2,28 @@ import java.io.*;
 
 public class Solution {
 
-    static int[] dy = {0, 1, 0, -1}; // 상하좌우 이동
+    static int[] dy = {0, 1, 0, -1}; // 우 하 좌 상
     static int[] dx = {1, 0, -1, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tcc = Integer.parseInt(br.readLine());
-
         for (int tc = 1; tc <= tcc; tc++) {
             int n = Integer.parseInt(br.readLine());
-            int dir = 0; // 방향
-            int count = 1; // 시작 숫자
-            int y = 0, x = 0; // 시작 위치
-            int[][] a = new int[n][n]; // n x n 배열 생성
+            int[][] a = new int[n][n];
 
-            while (count <= n * n) {
-                a[y][x] = count++; // 현재 위치에 값 할당
-                int ny = y + dy[dir]; // 다음 y 위치
-                int nx = x + dx[dir]; // 다음 x 위치
+            int y = 0;
+            int x = 0;
+            int dir = 0;
+            int count = 1;
 
-                // 배열 경계를 벗어나거나 이미 값이 할당된 경우 방향 전환
-                if (ny < 0 || ny >= n || nx < 0 || nx >= n || a[ny][nx] != 0) {
-                    dir = (dir + 1) % 4; // 방향 전환
+            while (count < n * n + 1) {
+                a[y][x] = count++;
+
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
+                if (ny < 0 || nx < 0 || ny > n - 1 || nx > n - 1 || a[ny][nx] != 0) {
+                    dir = (dir + 1) % 4;
                     ny = y + dy[dir];
                     nx = x + dx[dir];
                 }
@@ -41,4 +41,5 @@ public class Solution {
             }
         }
     }
+
 }
