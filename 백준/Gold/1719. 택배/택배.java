303680ee9b780bf51aae68168538d6adb855c18b
minseo Jo <liging12@naver.com>
2024-11-12 22:17:28 +0900

    [Gold III] Title: 택배, Time: 604 ms, Memory: 25356 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -1,48 +1,39 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 
-    static int MAX = 200 * 10000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
+        int[][] answer = new int[n + 1][n + 1];
         int[][] dist = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j) dist[i][j] = 0;
-                else dist[i][j] = MAX;
+                dist[i][j] = 987654321;
+                answer[i][j] = j;
             }
         }
 
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-            dist[u][v] = Math.min(dist[u][v], cost);
-            dist[v][u] = Math.min(dist[v][u], cost);
+            int c = Integer.parseInt(st.nextToken());
+            dist[u][v] = c;
+            dist[v][u] = c;
         }
 
-        int[][] result = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) result[i][j] = 0;
-                else result[i][j] = j;
-            }
-        }
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
-                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
                         dist[i][j] = dist[i][k] + dist[k][j];
-                        result[i][j] = result[i][k];
+                        answer[i][j] = answer[i][k];
                     }
                 }
             }
@@ -51,7 +42,7 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == j) System.out.print("- ");
-                else System.out.print(result[i][j] + " ");
+                else System.out.print(answer[i][j] + " ");
             }
             System.out.println();
         }
