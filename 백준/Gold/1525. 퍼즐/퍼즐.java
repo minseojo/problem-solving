fb1c79efc685371bfd843b00591228d78000370f
minseo Jo <liging12@naver.com>
2024-11-12 21:19:45 +0900

    [Gold II] Title: 퍼즐, Time: 804 ms, Memory: 113324 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -4,136 +4,66 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static int n = 3;
+    static final int N = 3;
+    static final String EXPECTED = "123456780";
     static Set<String> visited = new HashSet<>();
-    static String EXPECT = "123456780";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
+        
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            while (st.hasMoreTokens()) {
                 sb.append(st.nextToken());
             }
         }
-
+        
         visited.add(sb.toString());
         System.out.println(bfs(sb));
     }
 
     static int bfs(StringBuilder sb) {
-        int result = 0;
-        Queue<StringBuilder> q = new LinkedList<>();
-        q.add(sb);
+        Queue<StringBuilder> queue = new LinkedList<>();
+        queue.add(sb);
+        int moves = 0;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            while (size-- > 0) {
-                StringBuilder s = q.poll();
-                if (s.toString().equals(EXPECT)) return result;
-                for (int i = 0; i < s.length(); i++) {
-                    if (s.charAt(i) == '0') {
-                        switch (i) {
-                            case 0:
-                                swapRight(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 1:
-                                swapLeft(s, i, q);
-                                swapRight(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 2:
-                                swapLeft(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 3:
-                                swapUp(s, i, q);
-                                swapRight(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 4:
-                                swapLeft(s, i, q);
-                                swapRight(s, i, q);
-                                swapUp(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 5:
-                                swapLeft(s, i, q);
-                                swapUp(s, i, q);
-                                swapDown(s, i, q);
-                                break;
-                            case 6:
-                                swapUp(s, i, q);
-                                swapRight(s, i, q);
-                                break;
-                            case 7:
-                                swapLeft(s, i, q);
-                                swapUp(s, i, q);
-                                swapRight(s, i, q);
-                                break;
-                            case 8:
-                                swapLeft(s, i, q);
-                                swapUp(s, i, q);
-                                break;
-                        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                StringBuilder current = queue.poll();
+                if (current.toString().equals(EXPECTED)) return moves;
+
+                int zeroIndex = current.indexOf("0");
+                for (int direction : new int[]{-1, 1, -3, 3}) {
+                    int swapIndex = zeroIndex + direction;
+                    if (isValidMove(zeroIndex, swapIndex)) {
+                        swapAndAddToQueue(current, zeroIndex, swapIndex, queue);
                     }
                 }
             }
-            result++;
+            moves++;
         }
-
         return -1;
     }
 
-    static void swapLeft(StringBuilder sb, int idx, Queue<StringBuilder> q) {
-        StringBuilder temp = new StringBuilder(sb);
-        char a = sb.charAt(idx - 1);
-        char b = sb.charAt(idx);
-        temp.setCharAt(idx - 1, b);
-        temp.setCharAt(idx, a);
-        if (!visited.contains(temp.toString())) {
-            q.add(temp);
-            visited.add(temp.toString());
-        }
+    static boolean isValidMove(int zeroIndex, int swapIndex) {
+        return swapIndex >= 0 && swapIndex < 9 &&
+               !(zeroIndex % N == 2 && swapIndex % N == 0) &&
+               !(zeroIndex % N == 0 && swapIndex % N == 2);
     }
 
-    static void swapRight(StringBuilder sb, int idx, Queue<StringBuilder> q) {
-        StringBuilder temp = new StringBuilder(sb);
-        char a = sb.charAt(idx + 1);
-        char b = sb.charAt(idx);
-        temp.setCharAt(idx + 1, b);
-        temp.setCharAt(idx, a);
-        if (!visited.contains(temp.toString())) {
-            q.add(temp);
-            visited.add(temp.toString());
-        }
-    }
+    static void swapAndAddToQueue(StringBuilder sb, int zeroIndex, int swapIndex, Queue<StringBuilder> queue) {
+        StringBuilder nextState = new StringBuilder(sb);
+        char temp = nextState.charAt(zeroIndex);
+        nextState.setCharAt(zeroIndex, nextState.charAt(swapIndex));
+        nextState.setCharAt(swapIndex, temp);
 
-    static void swapUp(StringBuilder sb, int idx, Queue<StringBuilder> q) {
-        StringBuilder temp = new StringBuilder(sb);
-        char a = sb.charAt(idx - 3);
-        char b = sb.charAt(idx);
-        temp.setCharAt(idx - 3, b);
-        temp.setCharAt(idx, a);
-        if (!visited.contains(temp.toString())) {
-            q.add(temp);
-            visited.add(temp.toString());
-        }
-    }
-
-    static void swapDown(StringBuilder sb, int idx, Queue<StringBuilder> q) {
-        StringBuilder temp = new StringBuilder(sb);
-        char a = sb.charAt(idx + 3);
-        char b = sb.charAt(idx);
-        temp.setCharAt(idx + 3, b);
-        temp.setCharAt(idx, a);
-        if (!visited.contains(temp.toString())) {
-            q.add(temp);
-            visited.add(temp.toString());
+        String nextStateStr = nextState.toString();
+        if (!visited.contains(nextStateStr)) {
+            queue.add(nextState);
+            visited.add(nextStateStr);
         }
     }
 }
