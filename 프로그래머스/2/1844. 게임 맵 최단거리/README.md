b75f35af685aec969cd2cb6a7d8042dae26df858
minseo Jo <liging12@naver.com>
2025-01-18 18:12:44 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.30 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
 
 ### 성능 요약
 
-메모리: 56.1 MB, 시간: 9.84 ms
+메모리: 4.14 MB, 시간: 0.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 13일 03:32:33
+2025년 01월 18일 18:12:38
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+#include<queue>
+#include<iostream>
+using namespace std;
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+vector<vector<int>> dist;
+bool visited[104][104];
+int solution(vector<vector<int>> maps)
+{
+    int answer = 0;
+    int max_x = maps[0].size();
+    int max_y = maps.size();
+    dist.assign(max_y, vector<int>(max_x, 0));
+    dist[0][0] = 1; // 추가 된 코드
+    queue<pair<int,int>> q;
+    q.push({0,0});  // 처음 시작점 (1,1)
+    while(!q.empty()){
+        auto cur_location = q.front();
+        q.pop();
+        int x = cur_location.second;
+        int y = cur_location.first;
+        visited[y][x] = true;
+        if(x == max_x-1 && y == max_y-1){
+            return dist[y][x];
+        }
+        for(int i = 0; i < 4; i++){
+if(x+dx[i] < 0 || y+dy[i] < 0 || x+dx[i] >= max_x ||y+dy[i] >= max_y||maps[y+dy[i]][x+dx[i]] == 0 || visited[y+dy[i]][x+dx[i]]==true ) continue;
+            visited[y+dy[i]][x+dx[i]] = true;
+            q.push({y+dy[i], x+dx[i]});
+            dist[y+dy[i]][x+dx[i]] = dist[y][x]+1;
+        }
+    }
+     if(dist[max_x-1][max_y-1] == 0)
+         return -1;
+    return answer;
+}
