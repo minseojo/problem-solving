09a1466303101a31d23280a32454d590ae77a415
minseo Jo <liging12@naver.com>
2025-02-02 12:37:10 +0900

    [Gold IV] Title: 좋다, Time: 156 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,50 +1,48 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] a = new int [n];
+        int[] numbers = new int[n];
+
         for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(a);
-
-        Map<Integer, Integer> numbers = new HashMap<>();
+        Arrays.sort(numbers);
         for (int i = 0; i < n; i++) {
-            numbers.put(a[i], numbers.getOrDefault(a[i], 0) + 1);
-        }
+            int target = numbers[i];
+            int l = 0;
+            int r = numbers.length - 1;
 
-        int answer = 0;
-        Set<Integer> visited = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            if (!visited.contains(a[i]) && check(i, a)) {
-                visited.add(a[i]);
-                answer += numbers.get(a[i]);
+            while (l < r) {
+                if (l == i) {
+                    l++;
+                    continue;
+                }
+                if (r == i) {
+                    r--;
+                    continue;
+                }
+                
+                int sum = numbers[l] + numbers[r];
+                if (sum == target) {
+                    answer++;
+                    break;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    r--;
+                }
             }
         }
 
         System.out.println(answer);
-    }
-
-    static boolean check(int idx, int[] a) {
-        int l = 0;
-        int r = a.length - 1;
-        int target = a[idx];
-
-        while (l < r) {
-            int sum = a[l] + a[r];
-            if (sum == target && l != idx && r != idx) return true;
-            else if (sum > target) r--;
-            else l++;
-        }
-
-        return false;
     }
 }
