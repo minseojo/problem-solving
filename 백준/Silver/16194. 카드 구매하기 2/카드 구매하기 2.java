a519fabbee00896c7da3471838bc75f745bee943
Minseo Jo <liging12@naver.com>
2025-03-15 15:47:17 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 132 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(n, a));
+    }
+
+    public static int solution(int n, int[] a) {
+        int[] dp = a.clone();
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+
+        return dp[n];
+    }
+}
