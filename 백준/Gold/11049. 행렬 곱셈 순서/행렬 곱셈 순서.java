973a38d878b70bb2256a7643b6358d18d84a0d83
Minseo Jo <liging12@naver.com>
2025-03-15 16:08:02 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 256 ms, Memory: 17292 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.java
@@ -1,49 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int[][] matrix;
-    private static int[][] dp;
 
-    static int INF = 987654321;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        int n = Integer.parseInt(st.nextToken());
-        matrix = new int[n][n];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            matrix[i][0] = a;
-            matrix[i][1] = b;
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[][] a = new int[n + 1][2]; // [index, (row=0 | column=1)]
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i][0] = Integer.parseInt(st.nextToken());
+            a[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new int[n + 1][n + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i == j) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = INF;
-                }
+        System.out.println(solution(n, a));
+    }
+
+    public static int solution(int n, int[][] a) {
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) dp[i][j] = 0;
+                else dp[i][j] = Integer.MAX_VALUE;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i; j++) {
-                int l = j;
-                int r = j + i;
+        for (int i = 1; i <= n; i++) {
+            for (int l = 1; l <= n - i; l++) {
+                int r = l + i;
                 for (int k = l; k < r; k++) {
-                    dp[l][r] = Math.min(dp[l][r], dp[l][k] + dp[k + 1][r] + matrix[l][0] * matrix[k][1] * matrix[r][1]);
+                    dp[l][r] = Math.min(dp[l][r], dp[l][k] + dp[k + 1][r] + a[l][0] * a[k][1] * a[r][1]);
                 }
             }
         }
 
-        System.out.println(dp[0][n - 1]);
+        return dp[1][n];
     }
 }
