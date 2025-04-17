1c8b39436f46342859939793ac3ea9e0b2f088a4
Minseo Jo <liging12@naver.com>
2025-04-17 22:35:08 +0900

    [Silver III] Title: 수강신청, Time: 976 ms, Memory: 67428 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -4,39 +4,24 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-    static class Student {
-        String id;
-        int index;
-
-        Student(String id, int index) {
-            this.id = id;
-            this.index = index;
-        }
-    }
-
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
 
-        Map<String, Student> studentIndexes = new HashMap<>(); // id, student
+        LinkedHashMap<String, Boolean> map = new LinkedHashMap<>();
         for (int i = 0; i < l; i++) {
             String id = br.readLine();
-            studentIndexes.put(id, new Student(id, i));
+            map.remove(id); // 기존에 존재하면 삭제하고
+            map.put(id, true); // 다시 넣기 → 가장 마지막에 위치
         }
 
-        List<Student> students = new ArrayList<>();
-        for (Student s : studentIndexes.values()) {
-            students.add(s);
+        int count = 0;
+        for (String id : map.keySet()) {
+            if (count++ == k) break;
+            System.out.println(id);
         }
-        students.sort((s1, s2) -> Integer.compare(s1.index, s2.index));
-
-        int maxSize = Math.min(students.size(), k);
-        for (int i = 0; i < maxSize; i++) {
-            System.out.println(students.get(i).id);
-        }
-
     }
 }
