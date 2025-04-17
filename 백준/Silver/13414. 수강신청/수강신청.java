1b3f7b2328da2e06393dd11896a2cee958b2e621
Minseo Jo <liging12@naver.com>
2025-04-17 22:33:01 +0900

    [Silver III] Title: 수강신청, Time: 1280 ms, Memory: 73220 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -5,26 +5,38 @@ import java.util.*;
 
 public class Main {
 
+    static class Student {
+        String id;
+        int index;
+
+        Student(String id, int index) {
+            this.id = id;
+            this.index = index;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
         int l = Integer.parseInt(st.nextToken());
 
-        Map<String, Integer> map = new TreeMap<>();
+        Map<String, Student> studentIndexes = new HashMap<>(); // id, student
         for (int i = 0; i < l; i++) {
-            String name = br.readLine();
-            map.put(name, i);
+            String id = br.readLine();
+            studentIndexes.put(id, new Student(id, i));
         }
 
-        List<String> keySet = new ArrayList<>(map.keySet());
-        keySet.sort((o1, o2) -> Integer.compare(map.get(o1), map.get(o2)));
-
-        int pos = 0;
-        for (String s : keySet) {
-            System.out.println(s);
-            pos++;
-            if (pos == k) break;
+        List<Student> students = new ArrayList<>();
+        for (Student s : studentIndexes.values()) {
+            students.add(s);
         }
+        students.sort((s1, s2) -> Integer.compare(s1.index, s2.index));
+
+        int maxSize = Math.min(students.size(), k);
+        for (int i = 0; i < maxSize; i++) {
+            System.out.println(students.get(i).id);
+        }
+
     }
 }
