a57d595f2e84b0474f75e0e2de39e83e91f0f0cd
Minseo Jo <liging12@naver.com>
2025-05-16 18:51:28 +0900

    [level 3] Title: 섬 연결하기, Time: 0.95 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 1.89 ms
+메모리: 85.4 MB, 시간: 0.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 20일 21:32:24
+2025년 05월 16일 18:51:26
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,36 +2,45 @@ import java.util.*;
 
 class Solution {
     
-    int[] parent = new int[100];
-    
-    int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+    private int find(int x, int[] parent) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x], parent);
     }
     
-    void merge(int u, int v) {
-        u = find(u);
-        v = find(v);
-        if (u != v) parent[u] = parent[v];
+    // 다리 연결 (정점 인덱스 작은게 부모)
+    private void merge(int u, int v, int[] parent) {
+        u = find(u, parent);
+        v = find(v, parent);
+        if (u < v) parent[v] = u;
+        else parent[u] = v;
     }
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        for (int i = 0; i < n; i++) {
+        
+        int[] parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             parent[i] = i;
         }
-        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
-        for (int[] c : costs) {
-            int u = c[0];
-            int v = c[1];
-            int cost = c[2];
-            
-            if (find(u) != find(v)) {
-                merge(u, v);
-                answer += cost;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((i, j) -> Integer.compare(i[2], j[2]));
+        for (int[] cost: costs) {
+            pq.add(new int[] {cost[0], cost[1], cost[2]});
+        }
+        
+        int sum = 0;
+        while (!pq.isEmpty()) {
+            int[] cost = pq.poll();
+            int u = cost[0];
+            int v = cost[1];
+            int c = cost[2];
+            if (find(u, parent) != find(v, parent)) {
+                merge(u, v, parent);
+                sum += c;
             }
         }
         
+        answer = sum;
         return answer;
     }
 }
