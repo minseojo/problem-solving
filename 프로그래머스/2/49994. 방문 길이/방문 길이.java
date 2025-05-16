7fd4145b5e344f12c0656f49d07d934e092d73d1
Minseo Jo <liging12@naver.com>
2025-05-16 19:06:02 +0900

    [level 2] Title: 방문 길이, Time: 19.17 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,36 +1,66 @@
+import java.util.*;
+
 class Solution {
+    
+    private static int MIN = -5;
+    private static int MAX = 5;
+    
+    class Point {
+        int x, y;
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Point)) return false;
+            Point p = (Point) o;
+            return this.x == p.x && this.y == p.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+    
     public int solution(String dirs) {
         int answer = 0;
-        int[][][] maps = new int[12][12][4];
-        int y = 5;
-        int x = 5;
-        for (int i = 0; i < dirs.length(); i++) {
-            char c = dirs.charAt(i);
-            if (c == 'U') {
-                if (y - 1 < 0) continue;
-                maps[--y][x][0] = 1;
-            } else if (c == 'D') {
-                if (y + 1 > 10) continue;
-                maps[++y][x][1] = 1;
-            } else if (c == 'R') {
-                if (x + 1 > 10) continue;
-                maps[y][++x][2] = 1;
-            } else if (c == 'L') {
-                if (x - 1 < 0) continue;
-                maps[y][--x][3] = 1;
+        
+        Set<String> visitedEdges = new HashSet<>();
+
+        int y = 0, x = 0;
+        for (char dir : dirs.toCharArray()) {
+            int ny = 0, nx = 0;
+            if (dir == 'U') {
+                ny = y - 1;
+                nx = x;
+            } else if (dir == 'D') {
+                ny = y + 1;
+                nx = x;
+            } else if (dir == 'R') {
+                ny = y;
+                nx = x + 1;
+            } else if (dir == 'L') {
+                ny = y;
+                nx = x - 1;
             }
-        }
-        // UDRL
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 0; j <= 10; j++) {
-                if (maps[i][j][0] == 1 && maps[i + 1][j][1] == 1) answer--;
-                if (maps[i][j][2] == 1 && maps[i][j - 1][3] == 1) answer--;
-                for (int k = 0; k < 4; k++) {
-                    if (maps[i][j][k] == 1) {
-                        answer++;
-                    }
-                }
+            
+            if (ny < MIN || nx < MIN || ny > MAX || nx > MAX) continue;
+            
+            Point node = new Point(ny, nx);
+            String edge1 = x + "," + y + ">" + nx + "," + ny;
+            String edge2 = nx + "," + ny + ">" + x + "," + y;
+            if (!visitedEdges.contains(edge1) && !visitedEdges.contains(edge2)) {
+                answer++;
             }
+            
+            visitedEdges.add(edge1);
+            visitedEdges.add(edge2);
+            
+            y = ny;
+            x = nx;
         }
         
         return answer;
