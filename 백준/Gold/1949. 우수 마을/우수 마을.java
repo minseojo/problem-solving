8f572fb51c833cb717216735e5ea05b6cd444626
Minseo Jo <liging12@naver.com>
2025-05-17 18:30:47 +0900

    [Gold II] Title: 우수 마을, Time: 236 ms, Memory: 24712 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.java
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -8,47 +8,48 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int[] A = new int[N + 1];
+        int n = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        int[] town = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            town[i] = Integer.parseInt(st.nextToken());
         }
 
-        Map<Integer, List<Integer>> adj = new HashMap<>();
-        for (int i = 1; i <= N; i++) {
-            adj.put(i, new ArrayList<>());
+        Map<Integer, List<Integer>> tree = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            tree.put(i, new ArrayList<>());
         }
-        for (int i = 0; i < N - 1; i++) {
+
+        for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int U = Integer.parseInt(st.nextToken());
-            int V = Integer.parseInt(st.nextToken());
-            adj.get(U).add(V);
-            adj.get(V).add(U);
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            tree.get(u).add(v);
+            tree.get(v).add(u);
         }
 
-        int[][] dp = new int[N + 1][2];
         int root = 1;
-        solution(root, dp, adj, A, new boolean[N + 1]);
+        int[][] dp = new int[n + 1][2]; // 선택/비선택
+        boolean[] visited = new boolean[n + 1];
+
+        DFS(root, town, dp, visited, tree);
         int answer = Math.max(dp[root][0], dp[root][1]);
         System.out.println(answer);
+
     }
 
-    public static void solution(int current, int[][] dp, Map<Integer, List<Integer>> adj, int[] A, boolean[] visited) {
-        visited[current] = true;
+    private static void DFS(int node, int[] town, int[][] dp, boolean[] visited, Map<Integer, List<Integer>> tree) {
+        visited[node] = true;
 
-        dp[current][0] = 0;
-        dp[current][1] = A[current];
-
-        for (int i = 0; i < adj.get(current).size(); i++) {
-            int next = adj.get(current).get(i);
-            if (visited[next]) continue;
-
-            solution(next, dp, adj, A, visited);
-
-            dp[current][0] += Math.max(dp[next][0], dp[next][1]);
-            dp[current][1] += dp[next][0];
+        dp[node][1] = town[node];
+        for (int child : tree.get(node)) {
+            if (!visited[child]) {
+                DFS(child, town, dp, visited, tree);
+                dp[node][0] += Math.max(dp[child][0], dp[child][1]);
+                dp[node][1] += dp[child][0];
+            }
         }
     }
+
 }
