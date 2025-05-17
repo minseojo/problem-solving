e747608965311326a883850695702ee1b03b2976
Minseo Jo <liging12@naver.com>
2025-05-17 16:47:53 +0900

    [Gold V] Title: 트리, Time: 108 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,56 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine(), " ");
 
-        List<List<Integer>> tree = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        Map<Integer, List<Integer>> tree = new HashMap<>();
+
         for (int i = 0; i < n; i++) {
-            tree.add(new ArrayList<>());
+            tree.put(i, new ArrayList<>());
         }
 
         int root = -1;
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(st.nextToken());
-            if (x == -1) root = i;
-            else {
+            if (x == -1) {
+               root = i;
+            } else {
                 tree.get(x).add(i);
             }
         }
 
+        boolean[] visited = new boolean[n];
         int deleteNode = Integer.parseInt(br.readLine());
-        if (root == deleteNode) {
-            System.out.println(0);
-        } else {
-            boolean[] visited = new boolean[n];
-            visited[deleteNode] = true;
-            System.out.println(dfs(root, tree, visited));
-        }
+        int answer = DFS(root, deleteNode, visited, tree);
+        System.out.println(answer);
     }
 
-    private static int dfs(int here, List<List<Integer>> tree, boolean[] visited) {
-        visited[here] = true;
 
-        if (tree.get(here).size() == 0) return 1;
-        else if (tree.get(here).size() == 1 && visited[tree.get(here).get(0)]) return 1;
+    private static int DFS(int node, int deleteNode, boolean[] visited, Map<Integer, List<Integer>> tree) {
+        visited[node] = true;
+        if (node == deleteNode) return 0; // 루트가 삭제 된 경우
+
         int result = 0;
-        for (int i = 0; i < tree.get(here).size(); i++) {
-            int next = tree.get(here).get(i);
+        int childCount = 0;
+        for (int child : tree.get(node)) {
+            if (child == deleteNode) continue;
 
-            if (!visited[next]) {
-                result += dfs(next, tree, visited);
+            if (!visited[child]) {
+                childCount++;
+                result += DFS(child, deleteNode, visited, tree);
             }
         }
 
+        if (childCount == 0) return result + 1;
         return result;
     }
+
 }
