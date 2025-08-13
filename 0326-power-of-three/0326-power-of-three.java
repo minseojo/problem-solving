63d8e26379267c2f74cdd01d2489f6480cf8ec56
Minseo Jo <liging12@naver.com>
2025-08-13 18:15:04 +0900

    Time: 9 ms (10.87%), Space: 44.5 MB (10.93%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+
+        long x = 1;
+        while (x <= n) {
+            if (x == n) return true;
+            x *= 3;
+        }
+
+        return false;
+    }
+}
